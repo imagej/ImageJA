7e3bbae44b0f067f5c757dd725f3d77f06668159
Johannes Schindelin <gene099@admins-Computer.local>
2005-10-19 19:11:55 +0200

    sensible merge is to merge with upstream...

--- merge.sh
+++ merge.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-git checkout -f cumul || exit 1
+git checkout cumul || exit 1
 
-for i in .git/refs/heads/*; do
-	name=$(basename $i)
-	if [ $name != cumul -a $name != tools ]; then
-		echo "Trying to merge $name"
-		git-resolve HEAD $name $name || exit 3
-	fi
-done
+git pull . master
 
