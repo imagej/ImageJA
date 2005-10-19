2a77a2651de34029d753e3d9531c3a265556fc70
Johannes Schindelin <gene099@admins-Computer.local>
2005-10-19 19:06:43 +0200

    ignore tools/

--- test-builds.sh
+++ test-builds.sh
@@ -13,9 +13,10 @@ fi
 for i in .git/refs/heads/*; do
 	name=$(basename $i)
 	if [ $name = "$waitFor" ]; then
+		echo "found $waitFor"
 		waitFor=""
 	fi
-	if [ -z "$waitFor" -a $name != cumul -a $name != master ]; then
+	if [ -z "$waitFor" -a $name != cumul -a $name != master -a $name != tools ]; then
 		echo "Compiling $name"
 		git checkout $name || exit 1
 		#sh .git/cleanup.sh || exit 2
