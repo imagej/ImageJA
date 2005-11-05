2b12a54907fe13aa992f1ee6a52c83e02b32220e
Johannes Schindelin <gene099@admins-Computer.local>
2005-11-05 12:56:16 +0100

    fix graft (forgot true parent)

--- make_graft.sh
+++ make_graft.sh
@@ -1,1 +1,1 @@
-mkdir .git/info; echo dfe0f9dd4f1be799ae49b4f431de68e445569aa5 03bab52afa9afc8b2f0339b4a8ead014633a626f > .git/info/grafts
+mkdir .git/info; echo dfe0f9dd4f1be799ae49b4f431de68e445569aa5 03bab52afa9afc8b2f0339b4a8ead014633a626f e4ffdfe589777d0db7713ebf23b4e418a856f13e > .git/info/grafts
