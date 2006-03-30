06cda674edc74f2ba62bb4e1ed1cd81631c209f9
Johannes Schindelin <gene099@wbgn013.biozentrum.uni-wuerzburg.de>
2006-03-30 14:40:33 +0200

    use wsync to update more often (daily?)

---
+++ daily-update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+perl $(dirname $0)/wsync.perl
+if [ -f .wsync-add ]; then
+	cat .wsync-add | tr "\n" "\0" | xargs -0r perl ~/my/tools/mac2unix.pl
+	cat .wsync-add | tr "\n" "\0" | xargs -0r git-update-index --add
+fi
+if [ -f .wsync-remove ]; then
+	cat .wsync-remove | tr "\n" "\0" | xargs git-ls-files | xargs -0r rm
+	cat .wsync-remove | tr "\n" "\0" | xargs git-ls-files | xargs -0r git-update-index --remove
+fi
+GIT_AUTHOR_NAME="Wayne Rasband" GIT_AUTHOR_EMAIL="wsr@nih.gov" git commit -m "updated $(date +"%d.%m.%Y %H:%M")"
