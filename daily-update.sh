ae33b44f7185d6c7f8d961a43627d67045ae177f
Johannes Schindelin <gene099@wbgn013.biozentrum.uni-wuerzburg.de>
2006-04-14 16:11:27 +0200

    push to itas if updated

--- daily-update.sh
+++ daily-update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-perl $(dirname $0)/wsync.perl
+cd $(dirname $0)/..
+perl tools/wsync.perl
 if [ -f .wsync-add ]; then
 	cat .wsync-add | tr "\n" "\0" | xargs -0r perl ~/my/tools/mac2unix.pl
 	cat .wsync-add | tr "\n" "\0" | xargs -0r git-update-index --add
@@ -9,4 +10,4 @@ if [ -f .wsync-remove ]; then
 	cat .wsync-remove | tr "\n" "\0" | xargs git-ls-files | xargs -0r rm
 	cat .wsync-remove | tr "\n" "\0" | xargs git-ls-files | xargs -0r git-update-index --remove
 fi
-GIT_AUTHOR_NAME="Wayne Rasband" GIT_AUTHOR_EMAIL="wsr@nih.gov" git commit -m "updated $(date +"%d.%m.%Y %H:%M")"
+GIT_AUTHOR_NAME="Wayne Rasband" GIT_AUTHOR_EMAIL="wsr@nih.gov" git commit -m "updated $(date +"%d.%m.%Y %H:%M")" && git push itas
