839ff20a958deb1017484cf32a6fa1faf986bc1f
dscho <dscho>
2006-08-12 10:53:40 +0000

    reinstate support for unpacked jars in the plugins/ folder

--- ij/Menus.java
+++ ij/Menus.java
@@ -789,9 +789,10 @@ public class Menus {
 	public static void installUserPlugin(String className, boolean force) {
 		Menu menu = pluginsMenu;
 		int slashIndex = className.indexOf('/');
+		String command = className;
 		if (slashIndex>0) {
 			String dir = className.substring(0, slashIndex);
-			className = className.substring(slashIndex+1, className.length());
+			command = className.substring(slashIndex+1, className.length());
 			//className = className.replace('/', '.');
 			if (submenu==null || !submenuName.equals(dir)) {
  				submenuName = dir;
@@ -803,13 +804,12 @@ public class Menus {
 			menu = submenu;
 		//IJ.write(dir + "  " + className);
 		}
-		String command = className.replace('_',' ');
-		command.trim();
+		command = command.replace('_',' ');
 
 		boolean itemExists = (pluginsTable.get(command)!=null);
-		pluginsTable.put(command, className);
 		if(force && itemExists)
 			return;
+		pluginsTable.put(command, className.replace('/', '.'));
 
 		if (!force && itemExists)  // duplicate command?
 			command = command + " Plugin";
