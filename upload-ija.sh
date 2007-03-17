6d5a4651259f4911ef17fbdc30d524e4ff21e2c5
Johannes Schindelin <gene099@wbgn013.biozentrum.uni-wuerzburg.de>
2007-03-17 21:31:42 +0100

    When uploading ImageJA, upload source and ij.jar, too
    Also say how to edit the release.

--- upload-ija.sh
+++ upload-ija.sh
@@ -1,2 +1,5 @@
 make signed-ij.jar
 scp signed-ij.jar dscho@shell.sf.net:imageja/htdocs/ij.jar
+git archive --format=tar --prefix=ij-src origin | gzip -9 > ij-src.tar.gz
+curl -T "{ij.jar,ij-src.tar.gz}" ftp://upload.sourceforge.net/incoming/
+echo w3m http://sourceforge.net/project/admin/editreleases.php
