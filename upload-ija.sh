248ba4aaf641bbda9e16dcbc21fae4834eaf61ea
Johannes Schindelin <gene099@wbgn013.biozentrum.uni-wuerzburg.de>
2007-03-17 21:50:39 +0100

    Upload ij-src.jar and fix the URL for FileReleases

--- upload-ija.sh
+++ upload-ija.sh
@@ -1,5 +1,6 @@
 make signed-ij.jar
 scp signed-ij.jar dscho@shell.sf.net:imageja/htdocs/ij.jar
-git archive --format=tar --prefix=ij-src origin | gzip -9 > ij-src.tar.gz
-curl -T "{ij.jar,ij-src.tar.gz}" ftp://upload.sourceforge.net/incoming/
-echo w3m http://sourceforge.net/project/admin/editreleases.php
+git archive --format=zip --prefix=ij-src/ origin > ij-src.jar
+curl -T "{ij.jar,ij-src.jar}" ftp://upload.sourceforge.net/incoming/
+echo w3m http://sourceforge.net/project/admin/editpackages.php?group_id=150609
+
