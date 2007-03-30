a06b0c26198e7bb81b141611c41afaa9707abce7
Johannes Schindelin <gene099@wbgn013.biozentrum.uni-wuerzburg.de>
2007-03-30 21:56:57 +0200

    Sourceforge.net needs version in filenames

--- upload-ija.sh
+++ upload-ija.sh
@@ -1,6 +1,11 @@
+if [ -z "$1" ]; then
+	echo "need a version"
+	exit 1
+fi
 make signed-ij.jar
 scp signed-ij.jar dscho@shell.sf.net:imageja/htdocs/ij.jar
-git archive --format=zip --prefix=ij-src/ origin > ij-src.jar
-curl -T "{ij.jar,ij-src.jar}" ftp://upload.sourceforge.net/incoming/
+git archive --format=zip --prefix=ij-src/ origin > ij-src-$1.jar
+mv ij.jar ij-$1.jar
+curl -T "{ij-$1.jar,ij-src-$1.jar}" ftp://upload.sourceforge.net/incoming/
 echo w3m http://sourceforge.net/project/admin/editpackages.php?group_id=150609
 
