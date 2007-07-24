369a26c096c3f9d7e17d47886db0957edddcf6d6
Johannes Schindelin <johannes.schindelin@gmx.de>
2007-07-24 01:49:27 +0100

    Adjust to separate cvs-imageja
    Update CVS with
    
    git config alias.cvscione '!cd /home/gene099/my/imagej/cvs-imageja; git --git-dir=$(pwd)/.git cvsexportcommit -c imageja^ imageja'

--- upload-ija.sh
+++ upload-ija.sh
@@ -3,13 +3,13 @@ if [ -z "$1" ]; then
 	echo "need a version"
 	exit 1
 fi
-my-fetch-origin.sh
+(cd ../cvs-imageja && my-fetch-origin.sh)
 git tag v"$1" origin
 git push vib
 git push dumbo
 git push orcz
 make signed-ij.jar
-scp signed-ij.jar dscho@shell.sf.net:imageja/htdocs/ij.jar
+rsync -vau signed-ij.jar dscho@shell.sf.net:imageja/htdocs/ij.jar
 git archive --format=zip --prefix=ij-src/ origin > ij-src-$1.jar
 mv ij.jar ij-$1.jar
 curl -T "{ij-$1.jar,ij-src-$1.jar}" ftp://upload.sourceforge.net/incoming/
