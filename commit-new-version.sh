2dea0777aa04c632738bb4f10f625c545a2287d3
Johannes Schindelin <johannes.schindelin@gmx.de>
2008-05-16 16:43:21 +0100

    Teach commit-new-version to download the ZIP and the commit message
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- commit-new-version.sh
+++ commit-new-version.sh
@@ -1,29 +1,82 @@
-if [ -z "$1" ]; then
-	echo "Need a zip file"
+#!/bin/sh
+
+BRANCHNAME=imagej
+UPSTREAM=orcz
+
+git rev-parse $BRANCHNAME >/dev/null 2>/dev/null || {
+	echo "No branch $BRANCHNAME yet!"
 	exit 1
+}
+
+if [ -z "$1" ]; then
+	URL=http://rsb.info.nih.gov/ij
+	SRC_URL=$URL/download/src
+	NOTES_URL=$URL/notes.html
+	VERSION="$(curl $SRC_URL/ | \
+		sed -n "s/^.*ij\([0-9a-z]*\)-src.zip.*$/\1/p" | \
+		tail -n 1)"
+	test ! -z $(git tag -l ij$VERSION) && {
+		echo "Already have $VERSION"
+		exit 0
+	}
+	ZIP=ij$VERSION-src.zip
+	test -f $ZIP || curl $SRC_URL/$ZIP > $ZIP || {
+		echo "Could not get $SRC_URL/$ZIP"
+		exit 1
+	}
+	NOTES=notes$VERSION.txt
+	test -f $NOTES || w3m -dump $NOTES_URL >$NOTES || {
+		echo "Could not get notes"
+		exit 1
+	}
+	DOTVERSION=$(echo $VERSION | sed "s/^./&./")
+	(cat $NOTES | \
+	 sed -n \
+		-e "s/^  • $DOTVERSION, /•&/" \
+		-e "/^•  • $DOTVERSION, /,\$p" |
+	 sed \
+		-e "/^  • /,\$d" \
+		-e "s/^•\(  • $DOTVERSION, \)/\1/" |
+	 sh "$0" $ZIP) || {
+		echo "Could not commit!"
+		exit 1
+	}
+	git tag ij$VERSION $BRANCHNAME
+	exit 0
 fi
+
 case "$1" in
 /*) zipfile="$1";;
 *) zipfile="$(pwd)/$1";;
 esac
 
-cd "$(dirname "$0")"/../.git || exit 2
-export GIT_DIR="$(pwd)"
-parent=$(git-rev-parse release) || exit 3
-export GIT_INDEX_FILE="$GIT_DIR/tmpIndex"
-mkdir tmpCommit || exit 1
-cd tmpCommit
-unzip $zipfile
-cd source
-export GIT_WORK_TREE="$(pwd)"
-find -type f -print0 | xargs -0 perl $HOME/my/tools/mac2unix.pl
-find -type f -print0 | xargs -0 git-update-index --add
-tree=$(git-write-tree)
-export GIT_AUTHOR_NAME="Wayne Rasband"
-export GIT_AUTHOR_EMAIL="wsr@nih.gov"
-commit=$(git-commit-tree $tree -p $parent)
-git-update-ref refs/heads/release $commit
-cd ../..
+MAC2UNIX="$(cd "$(dirname "$0")" && pwd)"/mac2unix.pl
+
+git fetch $UPSTREAM $BRANCHNAME &&
+git push . FETCH_HEAD:$BRANCHNAME || {
+	echo "Could not update $BRANCHNAME to $UPSTREAM"
+	exit 1
+}
+parent=$(git-rev-parse $BRANCHNAME) || {
+	echo "Could not get revision for $BRANCHNAME"
+	exit 1
+}
+export GIT_DIR="$(cd "$(git rev-parse --git-dir)" && pwd)"
+export GIT_INDEX_FILE="$(pwd)/tmpIndex"
+mkdir tmpCommit || {
+	echo "tmpCommit/ already exists?"
+	exit 1
+}
+cd tmpCommit &&
+unzip $zipfile &&
+cd source &&
+export GIT_WORK_TREE="$(pwd)" &&
+find -type f -print0 | xargs -0 perl "$MAC2UNIX" &&
+find -type f -print0 | xargs -0 git-update-index --add &&
+tree=$(git-write-tree) &&
+export GIT_AUTHOR_NAME="Wayne Rasband" &&
+export GIT_AUTHOR_EMAIL="wsr@nih.gov" &&
+commit=$(git-commit-tree $tree -p $parent) &&
+git-update-ref refs/heads/$BRANCHNAME $commit &&
+cd ../.. &&
 rm -rf tmpCommit tmpIndex
-
-
