1bf79e5159558537974d39a1ee8a369675e18531
Johannes Schindelin <johannes.schindelin@gmx.de>
2008-05-16 16:55:45 +0100

    upload-ija: determine version automatically
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- upload-ija.sh
+++ upload-ija.sh
@@ -1,17 +1,20 @@
-set -x
+#!/bin/sh
+
+set -e
 if [ -z "$1" ]; then
-	echo "need a version"
-	exit 1
+	VERSION=$(git tag -l "ij*" | sort | tail -n 1 | sed "s/^ij\(.\)/\1./")
+else
+	VERSION="$VERSION"
 fi
 (cd ../cvs-imageja && my-fetch-origin.sh)
-git tag v"$1" origin
+git tag v"$VERSION" origin
 git push vib
 git push dumbo
-git push orcz imageja:master imageja v"$1"
+git push orcz imageja:master imageja v"$VERSION"
 make signed-ij.jar
 rsync -vau signed-ij.jar dscho@shell.sf.net:imageja/htdocs/ij.jar
-git archive --format=zip --prefix=ij-src/ origin > ij-src-$1.jar
-mv ij.jar ij-$1.jar
-curl -T "{ij-$1.jar,ij-src-$1.jar}" ftp://upload.sourceforge.net/incoming/
+git archive --format=zip --prefix=ij-src/ origin > ij-src-$VERSION.jar
+mv ij.jar ij-$VERSION.jar
+curl -T "{ij-$VERSION.jar,ij-src-$VERSION.jar}" ftp://upload.sourceforge.net/incoming/
 echo w3m http://sourceforge.net/project/admin/editpackages.php?group_id=150609
 
