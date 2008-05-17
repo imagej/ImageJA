bb043103b7f9cd527f9589f1b45fb9a0b88b704b
Johannes Schindelin <johannes.schindelin@gmx.de>
2008-05-17 12:56:26 +0200

    Add a script to conveniently merge with ImageJ
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ merge-imagej.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+URL=git://repo.or.cz/imageja.git
+BRANCH=refs/heads/imageja
+
+grep Sync .git/hooks/post-merge >/dev/null 2>/dev/null || {
+	echo "post-merge hook not installed properly!"
+	PWD="$(cd "$(dirname "$0")"; pwd)"
+	echo "Try 'ln -s $PWD/post-merge .git/hooks/post-merge"
+	exit 1
+}
+
+test $BRANCH != "$(git symbolic-ref HEAD)" && {
+	echo "Must be on branch $BRANCH"
+	exit 1
+}
+
+git fetch $URL $BRANCH || {
+	echo "No branch orcz?"
+	exit 1
+}
+
+test $(git rev-parse $BRANCH) != $(git rev-parse FETCH_HEAD) && {
+	echo "Branch $BRANCH is not up-to-date!"
+	exit 1
+}
+
+git merge imagej
+
