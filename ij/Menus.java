7f74033eed8756fb534eb5bcceb92685c0632faf
Johannes Schindelin <johannes.schindelin@gmx.de>
2008-05-20 02:00:18 +0100

    Reinstate plugins from jars
    In the endeavor to produce a headless Menus.java, I was a little bit
    too overzealous.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- ij/Menus.java
+++ ij/Menus.java
@@ -406,6 +406,7 @@ public class Menus {
             addJarErrorHeading(jar);
 			jarError += "    Duplicate command: " + s + "\n";
 		}
+		addPluginItem(s);
 		duplicateCommand = false;
     }
     
