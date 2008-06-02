5b28e6a4cc0f89442bf9ef3e5433b9c0c067d6ca
Johannes Schindelin <johannes.schindelin@gmx.de>
2008-06-03 01:22:04 +0200

    upload-ija: be user-agnostic
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- upload-ija.sh
+++ upload-ija.sh
@@ -57,9 +57,9 @@ git tag v"$VERSION" imageja
 git push dumbo
 git push orcz imageja:master imageja v"$VERSION"
 make signed-ij.jar
-rsync -vau signed-ij.jar dscho@shell.sf.net:imageja/htdocs/ij.jar
+rsync -vau signed-ij.jar shell.sf.net:imageja/htdocs/ij.jar
 git archive --format=zip --prefix=ij-src/ imageja > ij-src-$VERSION.jar
 mv ij.jar ij-$VERSION.jar
-rsync -e ssh -avP ij-$VERSION.jar ij-src-$VERSION.jar dscho@frs.sf.net:uploads/
+rsync -e ssh -avP ij-$VERSION.jar ij-src-$VERSION.jar frs.sf.net:uploads/
 echo w3m http://sourceforge.net/project/admin/editpackages.php?group_id=150609
 
