01cb0620911bcac327c10235ba0c684546114d62
Johannes Schindelin <johannes.schindelin@gmx.de>
2008-07-04 02:23:47 +0200

    merge-imagej: fix version number discovery when merge failed
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- merge-imagej.sh
+++ merge-imagej.sh
@@ -27,7 +27,7 @@ test $(git rev-parse $BRANCH) != $(git rev-parse FETCH_HEAD) && {
 }
 
 git merge imagej || {
-	VERSION="$(git log -1 --pretty=format:%s HEAD^2 |
+	VERSION="$(git log -1 --pretty=format:%s HEAD^2 imagej |
 		sed -n "s/^[^0-9]*\([\\.0-9]*.\),.*$/\1/p")"
 	echo "Sync with ImageJ $VERSION" > .git/MERGE_MSG
 	exit 1
