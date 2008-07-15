1bba71dafc7e47f53611dbe9d9c099960d39a0b8
Plugin Downloader <plugin.downloader@fiji>
2008-07-15 04:45:33 +0200

    Radial Profile Plot
        Author:       Paul Baggethun,
                      an engineer in Pittsburgh, PA
    
        Date:         2002/05/01
    
        Source:       Radial_Profile.java
    
        Installation: Download Radial_Profile.class to the plugins folder
                      and restart ImageJ.
    
        Description:  This plugin produces a profile plot of normalized
                      integrated intensities around concentric circles as a
                      function of distance from a point in the image. The
                      point is automatically defined as the center of the
                      rectangle that bounds the current ROI. The position
                      of this point can be modified in a dialog box.
    
                      The intensity at any given distance from the point
                      represents the sum of the pixel values around a
                      circle. This circle has the point as its center and
                      the distance from the point as radius. The integrated
                      intensity is divided by the number of pixels in the
                      circle that is also part of the image, yielding
                      normalized comparable values. The profile x-axis can
                      be plotted as pixel values or as values according to
                      the spatial calibration of input image.
    
                      Radial profiles are useful for measurement of X-ray
                      powder diffraction patterns as well as electron
                      diffraction patterns.
    
        See Also:     Radial Profile Extended plugin by Philippe Carl
    
    |Plugins | Home |

---
+++ download/Radial_Profile.java
@@ -0,0 +1,105 @@
+import ij.*;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.*;
+import ij.gui.*;
+import ij.measure.Calibration;
+import java.awt.*;
+import java.util.*;
+
+public class Radial_Profile implements PlugInFilter {
+
+	ImagePlus imp;
+	boolean canceled=false;
+	double X0;
+	double Y0;
+	double mR;
+	Rectangle rct;
+	int nBins=100;
+	//static boolean doNormalize = true;
+	static boolean useCalibration = false;
+	PlotWindow pw;
+
+	public int setup(String arg, ImagePlus imp) {
+		this.imp = imp;
+		return DOES_ALL+NO_UNDO+ROI_REQUIRED;
+	}
+
+	public void run(ImageProcessor ip) {
+		setXYcenter();
+		IJ.makeOval((int)(X0-mR), (int)(Y0-mR), (int)(2*mR), (int)(2*mR));
+		doDialog();
+		IJ.makeOval((int)(X0-mR), (int)(Y0-mR), (int)(2*mR), (int)(2*mR));
+		imp.startTiming();
+		if (canceled) return;
+		doRadialDistribution(ip);
+	}
+	
+	private void setXYcenter() {
+		rct = imp.getRoi().getBoundingRect();
+		X0 = (double)rct.x+(double)rct.width/2;
+		Y0 =  (double)rct.y+(double)rct.height/2;
+		mR =  (rct.width+rct.height)/4.0;
+	}
+
+	private void doRadialDistribution(ImageProcessor ip) {
+		nBins = (int) (3*mR/4);
+		int thisBin;
+		float[][] Accumulator = new float[2][nBins];
+		double R;
+		double xmin=X0-mR, xmax=X0+mR, ymin=Y0-mR, ymax=Y0+mR;
+		for (double i=xmin; i<xmax; i++) {
+			for (double j=ymin; j<ymax; j++) {
+				R = Math.sqrt((i-X0)*(i-X0)+(j-Y0)*(j-Y0));
+				thisBin = (int) Math.floor((R/mR)*(double)nBins);
+				if (thisBin==0) thisBin=1;
+				thisBin=thisBin-1;
+				if (thisBin>nBins-1) thisBin=nBins-1;
+				Accumulator[0][thisBin]=Accumulator[0][thisBin]+1;
+				Accumulator[1][thisBin]=Accumulator[1][thisBin]+ip.getPixelValue((int)i,(int)j);
+			}
+		}
+		Calibration cal = imp.getCalibration();
+		if (cal.getUnit() == "pixel") useCalibration=false;
+		if (useCalibration) {
+			for (int i=0; i<nBins;i++) {
+				Accumulator[1][i] =  Accumulator[1][i] / Accumulator[0][i];
+				Accumulator[0][i] = (float)(cal.pixelWidth*mR*((double)(i+1)/nBins));
+			}
+			pw = new PlotWindow("Radial Profile Plot", "Radius ["+cal.getUnits()+"]", "Normalized Integrated Intensity",  Accumulator[0], Accumulator[1]);
+		} else {
+			for (int i=0; i<nBins;i++) {
+				Accumulator[1][i] = Accumulator[1][i] / Accumulator[0][i];
+				Accumulator[0][i] = (float)(mR*((double)(i+1)/nBins));
+			}
+			pw = new PlotWindow("Radial Profile Plot", "Radius [pixels]", "Normalized Integrated Intensity",  Accumulator[0], Accumulator[1]);
+		}
+		pw.draw();
+	}
+
+	private void doDialog() {
+		canceled=false;
+		GenericDialog gd = new GenericDialog("Radial Distribution...", IJ.getInstance());
+		gd.addNumericField("X center (pixels):",X0,2);
+		gd.addNumericField("Y center (pixels):", Y0,2);
+		gd.addNumericField("Radius (pixels):", mR,2);
+		//gd.addCheckbox("Normalize", doNormalize);
+		gd.addCheckbox("Use Spatial Calibration", useCalibration);
+		gd.showDialog();
+		if (gd.wasCanceled()) {
+			canceled = true;
+			return;
+		}
+		X0=gd.getNextNumber();
+		Y0=gd.getNextNumber();
+		mR=gd.getNextNumber();
+		//doNormalize = gd.getNextBoolean();
+		useCalibration = gd.getNextBoolean();
+		if(gd.invalidNumber()) {
+			IJ.showMessage("Error", "Invalid input Number");
+			canceled=true;
+			return;
+		}
+	}
+}
+
+
