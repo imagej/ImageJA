0f3ba25efce0da5b3ffc5af17873ec2e69ea6580
Plugin Downloader <plugin.downloader@fiji>
2009-05-07 21:01:41 +0200

    QuickTime Movie Writer
        Author:       Wayne Rasband (wsr at nih.gov)
    
        Requires:     QuickTime and QuickTime for Java
    
        Limitations:  Does not work with 64-bit versions of Java. File names cannot be longer
                      that 32 characters.
    
        Bugs:         Saving in a folder with a name longer than 31 characters can cause a
                      QuickTime -47 (fBsyErr) error.
    
        Source:       QuickTime_Writer.java
    
        Installation: Download QuickTime_Plugins.jar to the plugins folder, or subfolder,
                      restart ImageJ and there will be a new File/Save As/QuickTime Movie
                      command. This JAR file also installs three other QuickTime plugins:
    
                        • QuickTime Capture (File/Import/Video)
                        • QT Movie Opener (File/Import/Using QuickTime)
                        • QT Player (File/Import/QuickTime Player)
    
                      Note that this JAR file is included with the Mac OS X version of
                      ImageJ, version 1.34 and later.
    
        Description:  Saves a stack as a QuickTime movie. Requires both QuickTime and
                      QuickTime for Java. Both are preinstalled on Macs. Windows users must
                      install QuickTime 7 and copy QTJava.zip to the Java extensions folder
                      (ImageJ\jre\lib\ext).
    
                      A dialog box allows the user to specify the type of compression used,
                      the quality, and the frame rate. The following compression methods
                      (codecs) are supported:
    
                        • Cinepak - a favorite in the early 90's and supported by many other
                          players, but quite obsolete today
                        • Animation - meant for synthetic images (i.e., graphics),
                          inappropriate for digitized images
                        • H.263 - pretty good videoconferencing codec, enjoys wide support
                          (it's the only useful video codec in JMF)
                        • Sorenson - long a QT favorite because of high compression and
                          attractive playback
                        • Sorenson 3 - an improved version of the Sorenson codec; requires
                          QuickTime 5
                        • MPEG-4 - massive compression; requires QuickTime 7
    
                      This plugin is based on the VideoSampleBuilder example in Chapter 8 of
                      QuickTime for Java: A Developer's Notebook by Chris Adamson. The codec
                      descriptions are from the MakeCSequence example in the Making Media
                      from Scratch article, also by Chris Adamson.
    
        History:      2000/10/20: First version
                      2005/10/11: Works with Java 1.4 on Mac OS X
                      2005/10/13: Fixed byte ordering bug on Windows
                      2005/11/09: Up to 8 times faster
                      2005/11/22: Compression ratio calculated correctly; options dialog
                      displayed before save as dialog
                      2007/05/22: Displays error if file name longer than 32 characters
                      2008/05/14: Minimum frame rate no longer limited to 0.1 fps
                      2009/05/07: Works with hyperstacks
    
        See Also:     The File>Save As>AVI command, which creates QuickTime compatible files,
                      using either no compression or M-JPEG compression
                      The File>Save As>Gif command, which saves an 8-bit stack as an animated
                      GIF
                      The QT Stack Writer plugin, which is faster and uses the standard
                      QuickTime compression dialog

--- download/QuickTime_Writer.java
+++ download/QuickTime_Writer.java
@@ -21,16 +21,20 @@ import java.io.*;
 // It is based on the VideoSampleBuilder example from chapter 8 of "QuickTime for Java: 
 // A Developer's Notebook" by Chris Adamson (www.oreilly.com/catalog/quicktimejvaadn/).
 public class QuickTime_Writer implements PlugIn, StdQTConstants {
-	static final int KEY_FRAME_RATE = 30;
 	static final int TIME_SCALE = 600;
 	String[] codecs = {"Cinepak", "Animation", "H.263", "Sorenson", "Sorenson 3", "MPEG-4"};
 	int[] codecTypes = {kCinepakCodecType, kAnimationCodecType, kH263CodecType, kSorensonCodecType, 0x53565133, 0x6d703476};
 	static String codec = "Sorenson";
 	String[] qualityStrings = {"Low", "Normal", "High", "Maximum"};
 	int[] qualityConstants = {codecLowQuality, codecNormalQuality, codecHighQuality, codecMaxQuality};
+	int keyFrameRate = 15;
 	static String quality = "Normal";
 
 	public void run(String arg) {
+		if (IJ.is64Bit() && IJ.isMacintosh()) {
+			IJ.error("This plugin requires a 32-bit version of Java");
+			return;
+		}
 		ImagePlus imp = IJ.getImage();
 		if (imp==null) return;
 		if (imp.getStackSize()==1) {
@@ -38,21 +42,15 @@ public class QuickTime_Writer implements PlugIn, StdQTConstants {
 			return;
 		}
 
-		SaveDialog sd = new SaveDialog("Save as QuickTime...", imp.getTitle(), ".mov");
-		String name = sd.getFileName();
-		if (name==null) return;
-		String dir = sd.getDirectory();
-		String path = dir+name;
-
 		Calibration cal = imp.getCalibration();
 		double fps = 7.0;
 		if (cal.frameInterval!=0.0)
 			fps = 1.0/cal.frameInterval;
-		int decimalPlaces = (int) fps == fps?0:1;
+		int decimalPlaces = (int) fps == fps?0:3;
 		GenericDialog gd = new GenericDialog("QuickTime Options");
 		gd.addChoice("Compression:", codecs, codec);
 		gd.addChoice("Quality:", qualityStrings, quality);
-		gd.addNumericField("Frame Rate:", fps, decimalPlaces, 3, "fps");
+		gd.addNumericField("Frame Rate:", fps, decimalPlaces, 4, "fps");
 		gd.showDialog();
 		if (gd.wasCanceled()) return;
 		codec = gd.getNextChoice();
@@ -67,11 +65,27 @@ public class QuickTime_Writer implements PlugIn, StdQTConstants {
 			if (quality.equals(qualityStrings[i]))
 				codecQuality = qualityConstants[i];
 		}
+		switch (codecQuality) {
+			case codecLowQuality: keyFrameRate=30; break;
+			case codecNormalQuality: keyFrameRate=15; break;
+			case codecHighQuality: keyFrameRate=7; break;
+			case codecMaxQuality: keyFrameRate=1; break;
+		}
 		fps = gd.getNextNumber();
-		if (fps<0.1) fps = 0.1;
+		if (fps<0.0016666667) fps = 0.0016666667; // 10 minutes/frame
 		if (fps>100.0) fps = 100.0;
 		int rate = (int)(TIME_SCALE/fps);
 		cal.frameInterval = 1.0/fps;
+
+		SaveDialog sd = new SaveDialog("Save as QuickTime...", imp.getTitle(), ".mov");
+		String name = sd.getFileName();
+		if (name==null) return;
+		if (name.length()>32) {
+			IJ.error("QuickTime Writer", "File name cannot be longer than 32 characters");
+			return;
+		}
+		String dir = sd.getDirectory();
+		String path = dir+name;
 
 		long start = System.currentTimeMillis();
 		try {
@@ -87,8 +101,9 @@ public class QuickTime_Writer implements PlugIn, StdQTConstants {
 
 		File f = new java.io.File(path);
 		double fsize = f.length();
-		int bytesPerPixel = imp.getBitDepth();
-		if (bytesPerPixel==24) bytesPerPixel = 32;
+		int bitsPerPixel = imp.getBitDepth();
+		if (bitsPerPixel ==24) bitsPerPixel = 32;
+		int bytesPerPixel = bitsPerPixel/8;
 		int isize = imp.getWidth()*imp.getHeight()*imp.getStackSize()*bytesPerPixel;
 		IJ.showStatus(IJ.d2s((System.currentTimeMillis()-start)/1000.0, 1)+ " seconds, "+IJ.d2s(isize/fsize,0)+":1 compression");
 	}
@@ -97,7 +112,7 @@ public class QuickTime_Writer implements PlugIn, StdQTConstants {
 		int width = imp.getWidth();
 		int height = imp.getHeight();
 		ImageStack stack = imp.getStack();
-		int frames = stack.getSize();
+		int images = stack.getSize();
 		QTFile movFile = new QTFile (new java.io.File(path));
 		Movie movie = Movie.createMovieFile(movFile, kMoviePlayer, createMovieFileDeleteCurFile|createMovieFileDontCreateResFile);
 		int timeScale = TIME_SCALE; // 100 units per second
@@ -115,14 +130,46 @@ public class QuickTime_Writer implements PlugIn, StdQTConstants {
 		imageHandle.lock();
 		RawEncodedImage compressedImage = RawEncodedImage.fromQTHandle(imageHandle);
 		CSequence seq = new CSequence(gw, bounds, gw.getPixMap().getPixelSize(), codecType, CodecComponent.bestFidelityCodec, 
-			codecQuality, codecQuality, KEY_FRAME_RATE, null, 0);
+			codecQuality, codecQuality, keyFrameRate, null, 0);
 		ImageDescription imgDesc = seq.getDescription();
 		int[] pixels2 = null;
-		for (int frame=1; frame<=frames; frame++) {
-			IJ.showProgress(frame+1, frames);
-			IJ.showStatus(frame+"/"+frames + " (" +IJ.d2s(frame*100.0/frames,0)+"%)");
-			ImageProcessor ip = stack.getProcessor(frame);
-			ip = ip.convertToRGB();
+		boolean hyperstack = imp.isHyperStack() || imp.isComposite();
+		boolean saveFrames=false, saveSlices=false, saveChannels=false;
+		int channels = imp.getNChannels();
+		int slices = imp.getNSlices();
+		int frames = imp.getNFrames();
+		int c = imp.getChannel();
+		int z = imp.getSlice();
+		int t = imp.getFrame();
+		if (hyperstack) {
+			if (frames>1) {
+				saveFrames = true;
+				images = frames;
+			} else if (slices>1) {
+				saveSlices = true;
+				images = slices;
+			} else if (channels>1) {
+				saveChannels = true;
+				images = channels;
+			} else
+				hyperstack = false;
+		}
+		for (int image=1; image<=images; image++) {
+			IJ.showProgress(image+1, images);
+			IJ.showStatus(image+"/"+images + " (" +IJ.d2s(image*100.0/images,0)+"%)");
+			ImageProcessor ip = null;
+			if (hyperstack) {
+				if (saveFrames)
+					imp.setPositionWithoutUpdate(c, z, image);
+				else if (saveSlices)
+					imp.setPositionWithoutUpdate(c, image, t);
+				else if (saveChannels)
+					imp.setPositionWithoutUpdate(image, z, t);
+				ip = new ColorProcessor(imp.getImage());
+			} else {
+				ip = stack.getProcessor(image);
+				ip = ip.convertToRGB();
+			}
 			int[] pixels = (int[])ip.getPixels();
 			RawEncodedImage pixelData = gw.getPixMap().getPixelData();
 			int intsPerRow = pixelData.getRowBytes()/4;
@@ -149,6 +196,7 @@ public class QuickTime_Writer implements PlugIn, StdQTConstants {
 		videoTrack.insertMedia (0, 0, videoMedia.getDuration(), 1);
 		OpenMovieFile omf = OpenMovieFile.asWrite (movFile);
 		movie.addResource (omf, movieInDataForkResID, movFile.getName());
+		if (hyperstack) imp.setPosition(c, z, t);
 	}
 
 	void printStackTrace(Exception e) {
@@ -156,6 +204,13 @@ public class QuickTime_Writer implements PlugIn, StdQTConstants {
 		PrintWriter pw = new PrintWriter(caw);
 		e.printStackTrace(pw);
 		String s = caw.toString();
+		if (s.indexOf("fBsyErr")!=-1) {
+			String msg =
+				"NOTE: Saving to a folder with a name longer than\n"+
+				"31 characters can cause this error. Overwriting\n"+
+				"an existing file can also cause it.\n \n";
+			s = msg+s;
+		}
 		new TextWindow("Exception", s, 500, 300);
 	}
 
