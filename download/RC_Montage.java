96d030ac06041499c42e05dbfde27846a960fd6f
Plugin Downloader <plugin.downloader@fiji>
2009-06-14 23:08:25 +0200

    RC Montage
        Author:       Julian Cooper (Julian.Cooper[at]uhb.nhs.uk)
    
        History:      2009/02/22: First version
    
        Source:       RC_montage.java
    
        Installation: Download RC_montage.class to the plugins folder and restart ImageJ or
                      click Help>Update Menus.
    
        Description:  This plugin is an alternative front end for the built-in Stacks>Make
                      Montage command. The essential difference is that the output montage
                      for a specified range of slices from a stack will fit a grid of
                      specified dimensions.
    
                      [dialog]
    
                      First Slice : the first slice to be displayed (default: 1).
    
                      Last Slice: the last slice to be displayed (default: last slice in the
                      stack).
    
                      Rows: the number of rows in the output montage (default: 5).
    
                      Columns: the number of columns in the output montage (default: 5).
    
                      Scale: the scale factor for each slice (default: 1).
    
                      Border width: the width in pixels of the border between each panel in
                      the output montage (default: 0, i.e. no border).
    
                      Add slice labels?: if true, slice labels are displayed on the output
                      montage (default: false).
    
                      Example:
                      246 slice CT image stack of an acetabular (hip socket) fracture to be
                      displayed as a 4x4 montage. Using the built-in Make Montage... command
                      takes some guesswork or calculation to fit the grid exactly. As the
                      increment is an integer quantity there is no guarantee that the last
                      slice of the range will appear in the montage. It is easy to produce a
                      montage where the range is too short, so that slices are not included
                      in the montage:
    
                      [too_short]
    
                      ...or too long, so that blank cells pad out the montage:
    
                      [too_long]
    
                      This plugin produces an output montaged exactly to 4x4 including the
                      first and last slices of the range:
    
                      [just_right]
    
    |Plugins | Home |

--- download/RC_Montage.java
+++ download/RC_Montage.java
@@ -1,9 +1,83 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL /ij/plugins/download/RC_Montage.java was not found on this server.</p>
-<hr>
-<address>Apache/2.2.3 (Red Hat) Server at rsb.info.nih.gov Port 80</address>
-</body></html>
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.*;
+import ij.plugin.*;
+
+/* 	Author: Julian Cooper
+	Contact: Julian.Cooper [at] uhb.nhs.uk
+	First version: 2009/02/22
+	Licence: Public Domain	*/
+
+/* This plugin creates a montage that best fits a desired grid
+*/
+public class RC_Montage implements PlugIn {
+
+	int first, last, rows, cols, border;
+	double scale;
+	boolean labels;
+
+	public void run(String arg) {
+		ImagePlus imp = WindowManager.getCurrentImage();
+		if (imp==null)
+			{IJ.noImage(); return;}
+		ImagePlus imp2 = imp.createImagePlus();
+		ImageStack stack = imp.getStack();
+		ImageStack stack2 = imp.createEmptyStack();
+		if (stack.getSize()==1)
+			{IJ.error("Stack Required"); return;}
+		if (!showDialog(stack))
+			return;
+		if (last>stack.getSize())
+			last = stack.getSize();
+		double n = last - first;
+		double rc = rows*cols;
+		if (rc < 2) {
+			cols = 2;
+			rc = 2;
+		}
+		double inc = n/(rc-1);
+
+		if (rc > n+1){
+			inc = 1;
+			rc = n+1;
+		}
+
+		int size = (int) rc;
+		int slice;
+		for(int i=0; i<size; i++) {
+			slice = (int) Math.rint(first + i * inc);
+			stack2.addSlice(stack.getSliceLabel(slice), stack.getProcessor(slice));
+		}
+		imp2.setStack(null, stack2);
+
+		MontageMaker mont = new MontageMaker();
+		mont.makeMontage(imp2, cols, rows, scale, 1, size, 1, border, labels);
+	}
+
+	public boolean showDialog(ImageStack stack) {
+		int n = stack.getSize();
+		GenericDialog gd = new GenericDialog("RC Montage");
+		gd.addNumericField("First Slice:", 1, 0);
+		gd.addNumericField("Last Slice:", n, 0);
+		gd.addNumericField("Rows:", 5, 0);
+		gd.addNumericField("Columns:", 5, 0);
+		gd.addNumericField("Scale:", 1.0, 2);
+		gd.addNumericField("Border width:", 0, 0);
+		gd.addCheckbox("Add slice labels?", labels);
+		gd.showDialog();
+		if (gd.wasCanceled())
+			return false;
+		first = (int) gd.getNextNumber();
+		last = (int) gd.getNextNumber();
+		rows = (int) gd.getNextNumber();
+		cols = (int) gd.getNextNumber();
+		scale = (double) gd.getNextNumber();
+		border = (int) gd.getNextNumber();
+		labels = gd.getNextBoolean();
+		return true;
+	}
+
+
+
+}
