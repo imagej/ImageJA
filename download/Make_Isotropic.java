ef2b9db0b03b64d7afe6ab275c7a8332fd031980
Plugin Downloader <plugin.downloader@fiji>
2009-06-14 22:56:56 +0200

    Make Isotropic
        Author:          Julian Cooper
                         Julian.Cooper[at]uhb.nhs.uk
    
        History:         2009/05/22 First version
    
        Source:          Make_Isotropic.java
    
        Installation:    Download Make_Isotropic.class to the plugins folder and restart ImageJ
                         or click Help > Update Menus.
    
        Dependency:      Requires the latest version of TransformJ (or at least
                         imagescience.jar) to be installed in the ImageJ plugins folder.
    
        Acknowledgement: Erik Meijering, author of TransformJ
    
        Description:     This plugin is designed to rescale an anisotropically calibrated stack
                         along the z-axis so the the pixel depth is the same as the pixel width.
                         This is useful to prepare a stack for use with plugins that work best
                         with isotropic voxel calibration such as TransformJ.
    
                         The plugin will return with an error message if the current image is
                         not a stack. It implicitly assumes that the pixel height is the same as
                         the pixel width. If the pixel depth is less than the pixel width then
                         there will be fewer slices in the output stack, which may result in
                         information loss.
    
    |Plugins | Home |

--- download/Make_Isotropic.java
+++ download/Make_Isotropic.java
@@ -1,9 +1,68 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL /ij/plugins/download/Make_Isotropic.java was not found on this server.</p>
-<hr>
-<address>Apache/2.2.3 (Red Hat) Server at rsb.info.nih.gov Port 80</address>
-</body></html>
+import ij.*;
+import ij.process.*;
+import ij.measure.*;
+import imagescience.image.Image;
+import imagescience.transform.Scale;
+import ij.plugin.filter.*;
+
+/* 	Author: Julian Cooper
+	Contact: Julian.Cooper [at] uhb.nhs.uk
+	First version: 2009/05/22
+	Licence: Public Domain	*/
+
+/*  Acknowledgements: Erik Meijering, author of TransformJ*/
+
+/*  This plugin scales an anisotropically calibrated stack of images so that the
+    calibration is isotropic (the pixel depth becomes the same as the pixel
+    width). The plugin will not rescale XY anisotropy.
+ 
+    Note: If the pixel depth of the source stack is less than then the width
+    there will be fewer slices in the output stack resulting in a potential loss
+    of information.
+*/
+
+/* Requires imagescience.jar to be installed in plugins folder
+ * Available as part of TransformJ written by Erik Meijering
+ * http://www.imagescience.org/meijering/software/transformj/
+ */
+
+
+
+public class Make_Isotropic implements PlugInFilter{
+    ImagePlus imp;
+    
+    public int setup(String arg, ImagePlus imp) {
+		this.imp = imp;
+		return DOES_ALL;
+	}
+
+    public void run(ImageProcessor ip) {
+
+        if(imp.getStackSize()==1) {
+            IJ.error("Stack required");
+            return;
+        }
+
+        ImagePlus sca=null;
+        Calibration cal = imp.getCalibration();
+        double scaling = cal.pixelDepth/cal.pixelWidth;
+        if(scaling!=1) {
+            sca = scale_TJ(imp, scaling);
+            sca.setTitle(imp.getTitle()+ " - isotropic");
+            sca.show();
+            sca.setCalibration(cal);
+            Calibration cal_sca = sca.getCalibration();
+            cal_sca.pixelDepth = cal_sca.pixelWidth;
+        } else {
+            IJ.showMessage("Make Isotropic", "Stack already isotropic");
+        }
+    }
+
+    ImagePlus scale_TJ(ImagePlus imp, double zscale) {
+        Image img = Image.wrap(imp);
+        final Scale scaler = new Scale();
+        final Image newimg = scaler.run(img, 1, 1, zscale,1,1, Scale.LINEAR);
+        ImagePlus newimp = newimg.imageplus();
+        return newimp;
+    }
+}
