6b2641dfa80bd722d7c0d6e29119d4d3dd099309
Plugin Downloader <plugin.downloader@fiji>
2009-09-17 13:01:50 +0200

    TIFF Dumper
        Author:       Curtis Rueden (ctrueden at wisc.edu)
    
        History:      2009/09/10: First version
                      2009/09/16: Better handling of IFDs with list entries
    
        Requires:     Bio-Formats plugin
    
        Source:       TIFF_Dumper.java
    
        Installation: Copy TIFF_Dumper.class to the plugins folder and restart ImageJ.
    
        Description:  Uses the Bio-Formats plugin to parse and output all TIFF tags for the
                      current image. Note that you have to temporarily remove the "_" from
                      the name of the Bio-Formats plugin ("loci_tools.jar") in order to
                      compile this plugin using Plugins>Compile and Run.
    
        See Also:       • The TIFF Tags plugin displays the tags of a specified TIFF file.
                        • The "Debug mode" option in Edit>Options>Misc, when enabled, causes
                          ImageJ to display tags when opening TIFF images.
                        • The Image>Show Info command and the getMetadata() macro function(),
                          in v1.43g or later, retrieve ASCII tags, such as "DateTime" and
                          "Software".
                        • Image>Show Info and getMetadata() retrieve the tags of TIFF images
                          opened using the Bio-Formats plugin.
    
    |Plugins | Home |

--- download/TIFF_Dumper.java
+++ download/TIFF_Dumper.java
@@ -3,47 +3,71 @@ import ij.plugin.*;
 import ij.io.FileInfo;
 import java.awt.Frame;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import loci.common.RandomAccessInputStream;
 import loci.formats.tiff.*;
 
 /** Parses and outputs all TIFF tags for the current image. */
 public class TIFF_Dumper implements PlugIn {
 
-	public void run(String arg) {
-		if (Menus.getCommands().get("Bio-Formats Importer")==null) {
-			IJ.error("TIFF Dumper", "Bio-Formats plugin required");
-			return;
-		}
-		ImagePlus imp = IJ.getImage();
-		FileInfo fi = imp.getOriginalFileInfo();
-		if (fi.directory==null || fi.fileFormat!=FileInfo.TIFF) {
-			IJ.error("TIFF Dumper", "File path not available or not TIFF file");
-			return;
-		}
-		String path = fi.directory + fi.fileName;
-		IJ.log("\\Clear");
-		IJ.log("PATH = "+path);
-		try {
-			dumpIFDs(path);
-		} catch(IOException e) {
-			IJ.error("Tiff Dumper", ""+e);
-		}
-		Frame log = WindowManager.getFrame("Log");
-		if (log!=null) log.toFront();
-	}
+ public void run(String arg) {
+   if (Menus.getCommands().get("Bio-Formats Importer")==null) {
+     IJ.error("TIFF Dumper", "Bio-Formats plugin required");
+     return;
+   }
+   ImagePlus imp = IJ.getImage();
+   FileInfo fi = imp.getOriginalFileInfo();
+   if (fi.directory==null || fi.fileFormat!=FileInfo.TIFF) {
+     IJ.error("TIFF Dumper", "File path not available or not TIFF file");
+     return;
+   }
+   String path = fi.directory + fi.fileName;
+   IJ.log("\\Clear");
+   IJ.log("PATH = "+path);
+   try {
+     dumpIFDs(path);
+   } catch(IOException e) {
+     IJ.error("Tiff Dumper", ""+e);
+   }
+   Frame log = WindowManager.getFrame("Log");
+   if (log!=null) log.toFront();
+ }
 
-	public static void dumpIFDs(String path) throws IOException {
-		RandomAccessInputStream in = new RandomAccessInputStream(path);
-		TiffParser parser = new TiffParser(in);
-		IFDList ifdList = parser.getIFDs();
-		for (IFD ifd : ifdList) {
-			for (Integer key : ifd.keySet()) {
-				int k = key.intValue();
-				String name = IFD.getIFDTagName(k);
-				Object value = ifd.getIFDValue(k);
-				IJ.log(name + " = " + value);
-			}
-		}
-	}
+ public static void dumpIFDs(String path) throws IOException {
+   IJ.showStatus("Parsing IFDs");
+   RandomAccessInputStream in = new RandomAccessInputStream(path);
+   TiffParser parser = new TiffParser(in);
+   IFDList ifdList = parser.getIFDs();
+   IJ.showStatus("");
+   for (IFD ifd : ifdList) {
+     for (Integer key : ifd.keySet()) {
+       int k = key.intValue();
+       String name = IFD.getIFDTagName(k);
+       String value = prettyValue(ifd.getIFDValue(k), 0);
+       IJ.log(name + " = " + value);
+     }
+   }
+   in.close();
+ }
+
+ private static String prettyValue(Object value, int indent) {
+   if (!value.getClass().isArray()) return value.toString();
+   char[] spaceChars = new char[indent];
+   Arrays.fill(spaceChars, ' ');
+   String spaces = new String(spaceChars);
+   StringBuilder sb = new StringBuilder();
+   sb.append("{\n");
+   for (int i=0; i<Array.getLength(value); i++) {
+     sb.append(spaces);
+     sb.append(" ");
+     Object component = Array.get(value, i);
+     sb.append(prettyValue(component, indent + 2));
+     sb.append("\n");
+   }
+   sb.append(spaces);
+   sb.append("}");
+   return sb.toString();
+ }
 
 }
