61226a6c0801cad1aef61d0534ee2036b91bccba
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2009-10-21 15:21:18 +0200

    Removed useless variable.

--- ij/gui/ImageCanvas.java
+++ ij/gui/ImageCanvas.java
@@ -246,8 +246,8 @@ public class ImageCanvas extends Canvas implements MouseListener, MouseMotionLis
 			g.setColor(showAllColor);
     }
     
-    void drawRoi(Graphics g, Roi roi, int index) {
-    	int type = roi.getType();
+    void drawRoi(Graphics g, Roi roi, int index) 
+    {
 		ImagePlus imp2 = roi.getImage();
 		roi.setImage(imp);
 		Color saveColor = roi.getLineColor();
