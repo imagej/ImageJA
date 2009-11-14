69cc8cc3e33ddcdf8a15cf87846c5b72af6b0281
Plugin Downloader <plugin.downloader@fiji>
2009-11-14 01:01:46 +0100

    Multi FDF Opener and Multi VFF Opener
        Authors:      Shanrong Zhang (Shanrong.Zhang at UTSouthwestern.edu)
                         Rogers Magnetic Resonance Center
                         Department of Radiology
                         University of Texas Southwestern Medical Center
                      Daniel Hornung (daniel.hornung at ds.mpg.de)
                         Biomedical Physics Group
                         Max-Planck-Institute for Dynamics and
                      Self-Organization
    
        History:      2004/01/06: First version
                      2009/11/13: Multi VFF Opener opens images from GEHC
                      microCT scanner
    
        Source:       Multi_VFF_Opener.java and Multi_FDF_Opener.java
    
        Installation: Download Multi_VFF_Opener.class and
                      Multi_FDF_Opener.class to the plugins folder, then
                      restart ImageJ.
    
        Description:  These plugins can be used to open multiple image
                      files from Varian MRI imaging system
                      ("Multi_FDF_Opener") and EVS 900 micro-CT system
                      ("Multi_VFF_Opener"). They are based upon Michael A.
                      Miller's pioneering efforts.
    
    |Plugins | Home |

--- download/Multi_VFF_Opener.java
+++ download/Multi_VFF_Opener.java
@@ -4,6 +4,8 @@
 //
 // This ImageJ plugin can be used to open MULTIPLE VFF format files as created by the EVS 900
 // microCT system. 
+// It should also work for images (2D and 3D) created by GEHC eXplore Locus SP
+// (tested with a few examples only).
 //
 // This program is free software; Therefore, you can redistribute it and/or modify it under the terms
 // of the GNU General Public License as published by the Free Software Foundation; either version 2
@@ -14,6 +16,8 @@
 //
 // Copyright (C) Shanrong Zhang <shanrong.zhang@utsouthwestern.edu>
 // Date: December 23, 2003
+// 
+// Edited in 2009 by Daniel Hornung <daniel.hornung@ds.mpg.de>
 //
 // ================================================================================
 
@@ -95,7 +99,7 @@ public class Multi_VFF_Opener implements PlugIn {
 		String subject = "NA";
 		long xdim = 0;
 		long ydim = 0;
-		long zdim = 0;
+		long zdim = 1; // any image is at least one layer thick
 		int rank = 0;
 		long bits = 0;
 		float xspacing = 0;
@@ -118,39 +122,48 @@ public class Multi_VFF_Opener implements PlugIn {
 			IJ.log("Parameters for " + vfffile + " : "); 
 			while ( ((line = in.readLine(  )) != null ) & !done ) {
 				IJ.log( "      VFF header>>> " + line);
-				if ( line.startsWith("subject") ) {
-					StringTokenizer st = new StringTokenizer(line, " =;");
-					st.nextToken();  // 1st token is 'subject'
-					subject = st.nextToken();
-				}
-				if ( line.startsWith("rank") ) {
-					StringTokenizer st = new StringTokenizer(line, " =;");
-					st.nextToken();  // 1st token is 'rank'
-					rank = Integer.valueOf(st.nextToken()).intValue();
-				}
-				if ( line.startsWith("bits") ) {
-					StringTokenizer st = new StringTokenizer(line, " =;");
-					st.nextToken();  // 1st token is 'bits'
-					bits = Long.valueOf(st.nextToken()).longValue();
-				}
-				if ( line.startsWith("elementsize") ) {
-					StringTokenizer st = new StringTokenizer(line, " =;");
-					st.nextToken();  // 1st token is 'elementsize'
-					elementsize = Float.valueOf(st.nextToken()).floatValue();
-				}
-				if ( line.startsWith("size") ) {
-					StringTokenizer st = new StringTokenizer(line, " =;");
-					st.nextToken();  // 1st token is 'size'
-					xdim = Long.valueOf(st.nextToken()).longValue();  // 2nd token is xdim
-					ydim = Long.valueOf(st.nextToken()).longValue();  // 3rd token is ydim
-					zdim = Long.valueOf(st.nextToken()).longValue();  // 4th token is zdim
-				}
-				if ( line.startsWith("spacing") ) {
-					StringTokenizer st = new StringTokenizer(line, " =;");
-					st.nextToken();  // 1st token is 'spacing'
-					xspacing = Float.valueOf(st.nextToken()).floatValue();  // 2nd token is xspacing
-					yspacing = Float.valueOf(st.nextToken()).floatValue();  // 3rd token is yspacing
-					zspacing = Float.valueOf(st.nextToken()).floatValue();  // 4th token is zspacing
+				try{
+					if ( line.startsWith("subject") ) {
+						StringTokenizer st = new StringTokenizer(line, " =;");
+						st.nextToken();  // 1st token is 'subject'
+						subject = st.nextToken();
+					}
+					if ( line.startsWith("rank") ) {
+						StringTokenizer st = new StringTokenizer(line, " =;");
+						st.nextToken();  // 1st token is 'rank'
+						rank = Integer.valueOf(st.nextToken()).intValue();
+					}
+					if ( line.startsWith("bits") ) {
+						StringTokenizer st = new StringTokenizer(line, " =;");
+						st.nextToken();  // 1st token is 'bits'
+						bits = Long.valueOf(st.nextToken()).longValue();
+					}
+					if ( line.startsWith("elementsize") ) {
+						StringTokenizer st = new StringTokenizer(line, " =;");
+						st.nextToken();  // 1st token is 'elementsize'
+						elementsize = Float.valueOf(st.nextToken()).floatValue();
+					}
+					if ( line.startsWith("size") ) {
+						StringTokenizer st = new StringTokenizer(line, " =;");
+						st.nextToken();  // 1st token is 'size'
+						xdim = Long.valueOf(st.nextToken()).longValue();  // 2nd token is xdim
+						ydim = Long.valueOf(st.nextToken()).longValue();  // 3rd token is ydim
+						zdim = Long.valueOf(st.nextToken()).longValue();  // 4th token is zdim
+					}
+					if ( line.startsWith("spacing") ) {
+						StringTokenizer st = new StringTokenizer(line, " =;");
+						st.nextToken();  // 1st token is 'spacing'
+						xspacing = Float.valueOf(st.nextToken()).floatValue();  // 2nd token is xspacing
+						yspacing = Float.valueOf(st.nextToken()).floatValue();  // 3rd token is yspacing
+						zspacing = Float.valueOf(st.nextToken()).floatValue();  // 4th token is zspacing
+					}
+					if (line.equals("\f")) {
+						// This (, form feed) seems to mark the last header
+						// line for vff files from GEHC microCT devices.
+						done = true;
+					}
+				} catch (NoSuchElementException nsee) {
+					// do nothing, not necessarily a fatal error
 				}
 				count = count + 1;
 				if ( count > 26 ) {
@@ -187,7 +200,9 @@ public class Multi_VFF_Opener implements PlugIn {
 		fi.pixelHeight = ysize;
 		fi.pixelDepth = zsize;
 		fi.intelByteOrder = false;
-		fi.fileType = FileInfo.GRAY16_SIGNED; 
+		fi.fileType = (bits == 8) ? FileInfo.GRAY8 : FileInfo.GRAY16_SIGNED;
+		IJ.log("      fi.fileType = "
+			   + ((bits == 8) ? "FileInfo.GRAY8" : "FileInfo.GRAY16_SIGNED"));
 		fi.unit = "mm";
 		fi.offset = (int)(file.length() - xdim*ydim*zdim*bits/8); // file size in bytes
 		IJ.log("      fi.offset = " + fi.offset);
