29ee5eeb74c2d07863daf3590354846212a7e7ec
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-11-16 22:35:22 +0100

    download-ijplugins: perform missing commits
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- download-ijplugins.sh
+++ download-ijplugins.sh
@@ -6,31 +6,42 @@ VERBOSE=t
 
 NEED_TO_PUSH=
 download_plugin () {
+	GIT_INDEX_FILE=.index
+	export GIT_INDEX_FILE
 	FILE="$1"
 	OPT=
 	case "$FILE" in */*) mkdir -p "$(dirname "$FILE")";; esac
 	test -f "$FILE" && OPT="--time-cond $FILE"
 	CONTENTS="$(eval curl --silent $OPT $BASE_URL/$FILE)"
-	test -z "$CONTENTS" && return
-	echo "$CONTENTS" > "$FILE"
+	if test -z "$CONTENTS"
+	then
+		if git rev-parse $BRANCH:$FILE > /dev/null 2>&1 &&
+			git diff --quiet "$FILE"
+		then
+			return
+		fi
+		CONTENTS="$(cat "$FILE")"
+	else
+		echo "$CONTENTS" > "$FILE"
+	fi
 	FILES="$(echo "$CONTENTS" | tr '\r' '\n' |
 	  sed -n '/Source:/,/Description:/s/.*a href="\([^"]*\)".*/\1/pi' |
 	  sort | uniq)"
 	test -z "$FILES" && return
 	test -z "$VERBOSE" || echo "Downloading $FILE" >&2
 
-	GIT_INDEX_FILE=.index
 	GIT_AUTHOR_NAME="Plugin Downloader"
 	GIT_AUTHOR_EMAIL="plugin.downloader@fiji"
 	GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 	GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
-	export GIT_INDEX_FILE GIT_AUTHOR_NAME GIT_AUTHOR_EMAIL \
+	export GIT_AUTHOR_NAME GIT_AUTHOR_EMAIL \
 		GIT_COMMITTER_NAME GIT_COMMITTER_EMAIL
 
 	PARENT=$(git rev-parse --verify $BRANCH 2> /dev/null)
 	test -z "$PARENT" || PARENT="-p $PARENT"
 
 	git read-tree $BRANCH 2> /dev/null
+	git add "$FILE" &&
 	echo "$FILES" |
 	while read f
 	do
@@ -46,9 +57,9 @@ download_plugin () {
 		test -z "$VERBOSE" || echo "Getting $f" >&2
 		sha1=$(curl --silent $(echo "$url" | sed "s/ /%20/g") |
 			git hash-object -w --stdin) || break
-		printf "100644 $sha1 0\t$f\n"
-	done | git update-index --index-info &&
-	git diff-index --cached --quiet HEAD && return
+		git update-index --add --cacheinfo 100644 $sha1 "$f" || break
+	done &&
+	git diff-index --cached --quiet $BRANCH && return
 	sha1=$(echo "$CONTENTS" |
 		w3m -cols 72 -dump -T text/html |
 		sed -e 1N -e '/^home | /d' |
