22cb887c5e152530e7dceb1292ac6b9f16cacb75
Wayne Rasband <wsr@nih.gov>
2010-03-11 20:30:38 +0100

    Backport Wayne's 16-bit TIFF fix
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- ij/io/ImageReader.java
+++ ij/io/ImageReader.java
@@ -149,7 +149,6 @@ public class ImageReader {
 		short[] pixels = new short[nPixels];
 		int base = 0;
 		short last = 0;
-		boolean useRowsPerStrip = fi.stripLengths[0]>nPixels*bytesPerPixel;
 		for (int k=0; k<fi.stripOffsets.length; k++) {
 			//IJ.log("seek: "+fi.stripOffsets[k]+" "+(in instanceof RandomAccessStream));
 			if (in instanceof RandomAccessStream)
@@ -158,10 +157,7 @@ public class ImageReader {
 				int skip = fi.stripOffsets[k] - fi.stripOffsets[k-1] - fi.stripLengths[k-1];
 				if (skip > 0) in.skip(skip);
 			}
-			int stripSize = fi.stripLengths[k];
-			if (useRowsPerStrip)
-				stripSize = fi.rowsPerStrip*fi.width*bytesPerPixel;
-			byte[] byteArray = new byte[stripSize];
+			byte[] byteArray = new byte[fi.stripLengths[k]];
 			int read = 0, left = byteArray.length;
 			while (left > 0) {
 				int r = in.read(byteArray, read, left);
@@ -346,16 +342,11 @@ public class ImageReader {
 						g = buffer[j++]&0xff;
 						r = buffer[j++]&0xff;
 						j++; // ignore alfa byte
-					} else if (fi.intelByteOrder) { // ARGB
-						r = buffer[j++]&0xff;
-						g = buffer[j++]&0xff;
-						b = buffer[j++]&0xff;
-						j++; // ignore alfa byte
 					} else { // ARGB
-						j++; // ignore alfa byte
 						r = buffer[j++]&0xff;
 						g = buffer[j++]&0xff;
 						b = buffer[j++]&0xff;
+						j++; // ignore alfa byte
 					}
 				} else {
 					r = buffer[j++]&0xff;
