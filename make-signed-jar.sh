fb2dc7ec5acd9a55d9c333715acc224a480b2660
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-03-23 09:52:44 +0100

    Try harder to find jarsigner
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- make-signed-jar.sh
+++ make-signed-jar.sh
@@ -2,4 +2,12 @@
 
 cd "$(dirname "$0")"
 
+if ! type jarsigner > /dev/null 2>&1
+then
+	test -x "$JAVA_HOME"/bin/jarsigner ||
+	JAVA_HOME="$(../fiji --print-java-home)"/.. ||
+	exit 1
+	export PATH=$PATH:"$JAVA_HOME"/bin
+fi
+
 jarsigner $(cat .jarsignerrc) -signedjar signed-ij.jar ij.jar dscho
