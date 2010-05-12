428b41e029bc523dd1496ff54317281b512f7cc5
Plugin Downloader <plugin.downloader@fiji>
2010-05-12 23:02:03 +0200

    MultiColumnDialog
        Author:       Wayne Rasband (wsr at nih.gov)
    
        History:      2010/052/11: Initial release
    
        Requires:     ImageJ 1.44a or later
    
        Source:       MultiColumnDialog.java
    
        Installation: Download MultiColumnDialog.java to the plugins folder and compile and
                      run it using the Plugins>Compile and Run command.
    
        Description:  This plugin demonstrates how to display checkbox groups in
                      GenericDialogs. It requires ImageJ 1.44a or later. The dialog box it
                      displays is modeled after the LOCI Bio-Formats Import Options dialog
                      box.
    
                      [dialog]
    
    |Plugins | Home |

--- MultiColumnDialog.java
+++ MultiColumnDialog.java
@@ -13,40 +13,63 @@ public class MultiColumnDialog implements PlugIn {
 	public void run(String arg) {
 		if (IJ.versionLessThan("1.44a"))
 			return;
+
+		// checkbox group 1
+		int rows1=4, columns1=2;
+		String[] headings1 = {
+			"Dataset Organization",
+			"Memory Management"};
+		String[] labels1 = {
+			"Group files with similar names",
+			"Use virtual stack",
+			"Open files individually",
+			"Record modifications",
+			"Swap dimensions",
+			"Crop on import",
+			"",  // leave blank
+			"Specify range for series"};
+		boolean[] states1 = {false,false,true,false,false,true,false, false};
+
+		// checkbox group 2
+		int rows2=4, columns2=3;
+		String[] headings2 = {
+			"Color Options",
+			"Split",
+			"Display"};
+		String[] labels2 = {
+			"Merge RGB",
+			"Channels",
+			"Metadata",
+			"Colorize",
+			"Focal planes",
+			"OME-XML",
+			"Swap channels",
+			"Timepoints",
+			"ROIs",
+			"Autoscale"};
+		boolean[] states2 = {false,true,true,false,false,true,false,false, false,true};
+
 		GenericDialog gd = new GenericDialog("Dialog With Many Options");
-		String[] labels = new String[8];
-		boolean[] states = new boolean[8];
-		String[] headings = new String[2];
-		headings[0] = "Dataset Organization";
-		headings[1] = "Memory Management";
-		labels[0]="Group files with similar names"; states[0]=false;
-		labels[1]="Use virtual stack"; states[1]=false;
-		labels[2]="Open files individually"; states[2]=true;
-		labels[3]="Record modifications"; states[3]=false;
-		labels[4]="Swap dimensions"; states[4]=false;
-		labels[5]="Crop on import"; states[5]=true;
-		labels[6]="Open all series"; states[6]=false;
-		labels[7]="Specify range for series"; states[7]=true;
-		gd.addCheckboxGroup(4, 2, labels, states, headings);
-		String[] labels2 = new String[12];
-		boolean[] states2 = new boolean[12];
-		String[] headings2 = new String[3];
-		headings2[0] = "Color Options";
-		headings2[1] = "Split";
-		headings2[2] = "Display";
-		labels2[0]="Merge RGB"; states2[0]=false;
-		labels2[1]="Channels"; states2[1]=false;
-		labels2[2]="Metadata"; states2[2]=true;
-		labels2[3]="Colorize"; states2[3]=true;
-		labels2[4]="Focal planes"; states2[4]=false;
-		labels2[5]="OME-XML"; states2[5]=false;
-		labels2[6]="Swap channels"; states2[6]=false;
-		labels2[7]="Timepoints"; states2[7]=true;
-		labels2[8]="ROIs"; states2[8]=false;
-		labels2[9]="Autoscale"; states2[9]=false;
+		gd.addCheckboxGroup(rows1, columns1, labels1, states1, headings1);
 		gd.setInsets(20, 0, 0 );
-		gd.addCheckboxGroup(4, 3, labels2, states2, headings2);
+		gd.addCheckboxGroup(rows2, columns2, labels2, states2, headings2);
 		gd.showDialog();
+		if (gd.wasCanceled())
+			return;
+		IJ.log("");
+		for (int i=0; i<labels1.length; i++) {
+			if (labels1[i].length()!=0) {
+				boolean b = gd.getNextBoolean();
+				IJ.log(labels1[i]+": "+b);
+			}
+		}
+		IJ.log("");
+		for (int i=0; i<labels2.length; i++) {
+			if (labels2[i].length()!=0) {
+				boolean b = gd.getNextBoolean();
+				IJ.log(labels2[i]+": "+b);
+			}
+		}
 	}
 
 }
