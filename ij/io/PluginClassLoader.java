a04651be35d23e403ff503fa5f51146a59e6b970
Dimiter Prodanov <dimiterpp@gmail.com>
2010-05-16 20:48:29 +0200

    Add addDir() and addFile() methods to the PluginClassLoader

--- ij/io/PluginClassLoader.java
+++ ij/io/PluginClassLoader.java
@@ -1,12 +1,11 @@
 package ij.io;
-
 import ij.IJ;
-
 import java.io.*;
 import java.net.*;
-
 import java.util.Enumeration;
 import java.util.Vector;
+
+
 
 /** ImageJ uses this class loader to load plugins and resources from the
  * plugins directory and immediate subdirectories. This class loader will
@@ -21,7 +20,7 @@ import java.util.Vector;
  * <p> The class loader does not recurse into subdirectories beyond the first level.
 */
 public class PluginClassLoader extends URLClassLoader {
-    protected String path;
+ //   protected String path;
 
     /**
      * Creates a new PluginClassLoader that searches in the directory path
@@ -30,30 +29,136 @@ public class PluginClassLoader extends URLClassLoader {
      * are stored in a Vector for future searches.
      * @param path the path to the plugins directory.
      */
-	public PluginClassLoader(String path) {
+ 	public PluginClassLoader(String path) {
 		super(new URL[0], IJ.class.getClassLoader());
-		init(path);
+		//init(path);
+		try {
+			addDir(  path, true);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	/** This version of the constructor is used when ImageJ is launched using Java WebStart. */
-	public PluginClassLoader(String path, boolean callSuper) {
+ 	public PluginClassLoader(String path, boolean callSuper) {
 		super(new URL[0], Thread.currentThread().getContextClassLoader());
-		init(path);
+		//init(path);
+		/*try {
+			addDir(  path, true);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
 	}
 
-	public PluginClassLoader(Vector urls) {
-		super(new URL[0], Thread.currentThread().getContextClassLoader());
-		Enumeration e = urls.elements();
-		while (e.hasMoreElements()) try {
-			String url = (String)e.nextElement();
-			addURL(new URL(url));
-		} catch (MalformedURLException e2) {
-			e2.printStackTrace();
-		}
-	}
+ 	protected Vector<String> filesv=new Vector<String>();
+	
+	   
 
+    public  void addFile (String path) throws MalformedURLException
+    {
+        String urlPath = "jar:file://" + path + "!/";
+        System.out.println("adding path: "+urlPath);
+        addURL (new URL (urlPath));
+       // addURL(f.toURI().toURL());
+        filesv.add(path);
+    }
+    
+    public String[] getJars() {
+    	String[] ret=new String[ filesv.size()];
+    	int cnt=0;
+    	Enumeration<String> e=filesv.elements();
+    	while ( e.hasMoreElements()) {
+    		ret[cnt]= e.nextElement();
+    		cnt++;
+    	}
+    	return ret;
+    	
+    }
+    
+    protected  void addDir(String path) throws MalformedURLException
+    {
+    	File f= new File(path);
+    	
+    	if (f.exists() && f.isDirectory()) {
+    		System.out.println("browsing " + f.getAbsolutePath());
+    		filesv.add(f.getAbsolutePath());
+    		
+    		//find all JAR files on the path and subdirectories
+    		 
+         
+            // Add plugin directory to search path
+            addURL(f.toURI().toURL());
+           
+    		
+    		File[] jars=f.listFiles(new JarFilter());
+    		
+    		for (int i=0; i<jars.length; i++) {
+    			 String urlPath = "jar:file://" + jars[i].getAbsolutePath() + "!/";
+    		        System.out.println("adding path: "+jars[i].getAbsolutePath());
+    		        //addURL (new URL (urlPath));
+    		        addURL(jars[i].toURI().toURL());
+    		        filesv.add(jars[i].getAbsolutePath());
+    		}
+    		
+    		File[]  zips=f.listFiles(new ZipFilter());
+    		
+    		for (int i=0; i<zips.length; i++) {
+    			 //String urlPath = "jar:file://" + jars[i].getAbsolutePath() + "!/";
+    		        System.out.println("adding path: "+zips[i].getAbsolutePath());
+    		       // addURL (new URL (urlPath));
+    		        addURL(zips[i].toURI().toURL());
+    		        filesv.add(zips[i].getAbsolutePath());
+    		}
+    		
+    		
+    	}
+       
+    }
+    
+    public  void addDir(String path, boolean recursive) throws MalformedURLException {
+    	addDir( path);
+    	if (recursive) {
+     
+    		File f= new File(path);
+	    	
+	    	if (f.exists() && f.isDirectory()) {
+	    		File[] flist= f.listFiles();
+	    		int  cnt=0;
+	    		for (int i=0; i<flist.length; i++) {
+	    			if (flist[i].isDirectory()) cnt++;
+	    		}
+	    		File[] dirlist=new File[cnt];
+	    		cnt=0;
+	    		for (int i=0; i<flist.length; i++) {
+	    			if (flist[i].isDirectory()) { 
+	    				dirlist[cnt]=flist[i];
+	    				cnt++;
+	    			}
+	    		}
+	    		for (int i=0; i<dirlist.length; i++) {
+	    			 
+	    			addDir(dirlist[i].getAbsolutePath(),  recursive);
+	    		}
+	    	}
+    	}
+    }
+    
+    public void addJars (String s[], int offset) throws MalformedURLException {
+    	if (offset<0) offset=0;
+    	if (offset>s.length-1) offset=0;
+    	for (int i=offset; i<s.length; i++) {
+    		addFile (s[i]);
+    	}
+    	 
+    }
+    
+    
+ /*
+	
 	void init(String path) {
-		this.path = path;
+		//this.path = path;
 
 		//find all JAR files on the path and subdirectories
 		File f = new File(path);
@@ -97,5 +202,24 @@ public class PluginClassLoader extends URLClassLoader {
             }
         }
     }
-
+ */
+    class JarFilter implements FilenameFilter {
+    	  final String pattern=".jar";
+    		@Override
+    		public boolean accept(File f, String name) {
+    			 return name.toLowerCase().endsWith(pattern.toLowerCase());
+    	 
+    		}
+    		  
+    }
+    
+    class ZipFilter implements FilenameFilter {
+  	  final String pattern=".zip";
+  		@Override
+  		public boolean accept(File f, String name) {
+  			 return name.toLowerCase().endsWith(pattern.toLowerCase());
+  	 
+  		}
+  		  
+  }
 }
