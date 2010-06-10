0479dc5d9971531292b1d3f35dcc0020835dd655
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-06-10 15:06:33 +0200

    ImageStack: fix addSlice() directly after new ImageStack() with depth > 0
    When an ImageStack was newly created, and you tried to add slices right
    away, due to an off-by-one bug, an exception was thrown instead.
    
    Noticed by Daniel Hornung.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- ij/ImageStack.java
+++ ij/ImageStack.java
@@ -56,12 +56,12 @@ public class ImageStack {
 		if (!pixels.getClass().isArray()) 
 			throw new IllegalArgumentException("'pixels' is not an array");
 		nSlices++;
-		if (nSlices==stack.length) {
+		if (nSlices>=stack.length) {
 			Object[] tmp1 = new Object[nSlices*2];
-			System.arraycopy(stack, 0, tmp1, 0, nSlices);
+			System.arraycopy(stack, 0, tmp1, 0, stack.length);
 			stack = tmp1;
 			String[] tmp2 = new String[nSlices*2];
-			System.arraycopy(label, 0, tmp2, 0, nSlices);
+			System.arraycopy(label, 0, tmp2, 0, label.length);
 			label = tmp2;
 		}
 		stack[nSlices-1] = pixels;
