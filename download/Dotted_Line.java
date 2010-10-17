f6c27fbb8fea11f1dca241d63181aa494935d68e
Plugin Downloader <plugin.downloader@fiji>
2010-10-17 13:01:43 +0200

    Dotted Line
        Author:       Kevin (Gali) Baler
    
        Date:         2003/11/10: First release
                      2010/09/30: Updated to use latest ImageJ API
    
        Source:       Dotted_Line.java
    
        Installation: Drag and drop Dotted_Line.class onto the "ImageJ" window.
    
        Description:  This plugin draws dashed lines based on the current selection. Users
                      can specify the color, line width, and the pattern of the dashes.
                      Choose the color by double clicking on the color picker tool and
                      selecting the desired color. The line width box sets the width of the
                      line. The dash pattern box takes the list of numbers and turns them
                      into dashes or spaces. The first number is the pixel length of the
                      first dash and the second number is the pixel length of the space.
    
                      Dotted LineTool.jpg
    
                      The example below shows some of the variations that can be made to the
                      dashed lines. The red dashes show examples of dashed lines with all the
                      tools. The blue dashes are examples of the various line width options
                      and the green dashes illustrate how the dash pattern works. The numbers
                      next to the blue and green dashes are the values that were used to draw
                      those dashes.
    
                      Dotted LineDemo.jpg
    
        Notes:        When making dashed lines, ImageJ converts the image to an RGB image. So
                      if a dotted line is drawn on an 8-bit image, the image turns into an
                      RGB image. With the undo command, both, the dashed line and the RGB
                      color will be undone. However, once more than a single line has been
                      drawn, the undo command will undo only the last line that was drawn but
                      the image will remain in RGB.
    
    |Plugins | Home |

--- download/Dotted_Line.java
+++ download/Dotted_Line.java
@@ -9,15 +9,22 @@ import java.awt.image.*;
 import java.util.*;
 
 public class Dotted_Line implements PlugIn {
-    static double width = 2;
+    static double width = 5;
     static String dashpattern = "12, 12";
     float[] dashvalues;
 
     public void run(String arg) {
-       ImagePlus imp = IJ.getImage();
-       if (imp.getRoi()==null)
-       IJ.error("Selection required");
-       else if (showDialog()) {
+       ImagePlus imp = WindowManager.getCurrentImage();
+       if (imp==null) {
+          imp = IJ.createImage("Untitled", "8-bit Ramp", 512, 512, 1);
+          imp.show();
+          imp.setRoi(new OvalRoi(63, 64, 364, 363));
+       }
+       if (imp.getRoi()==null) {
+          IJ.error("Selection required");
+          return;
+       }
+       if (showDialog()) {
           Undo.setup(Undo.TYPE_CONVERSION, imp);
           drawLine(imp, width, dashvalues);
        }
@@ -54,77 +61,66 @@ public class Dotted_Line implements PlugIn {
        int type = roi.getType();
        Rectangle r = roi.getBounds();
        switch (type) {
-          case 0 :
-             //Rectangular Selections
+          case Roi.RECTANGLE:
              Rectangle2D rect = new Rectangle2D.Double(r.x, r.y, r.width, r.height);
              g2.draw(rect);
              break;
-          case 1 :
-             //Oval Selections
+          case Roi.OVAL:
              Ellipse2D ellipse = new Ellipse2D.Double(r.x, r.y, r.width, r.height);
              g2.draw(ellipse);
              break;
-          case 2 :
-             //Polygon Selections
-             PolygonRoi pr = (PolygonRoi) roi;
+          case Roi.POLYGON:
+              PolygonRoi pr = (PolygonRoi) roi;
              int x1Points[] = pr.getXCoordinates();
              int y1Points[] = pr.getYCoordinates();
              int Length = pr.getNCoordinates();
              GeneralPath polygon = new GeneralPath(GeneralPath.WIND_EVEN_ODD, Length);
              polygon.moveTo((x1Points[0] + r.x), (y1Points[0] + r.y));
-             for (int index = 1; index < Length; index++) {
+             for (int index = 1; index < Length; index++)
                 polygon.lineTo((x1Points[index] + r.x), (y1Points[index] + r.y));
-             };
              polygon.closePath();
              g2.draw(polygon);
              break;
-          case 3 :
-             //Freehand Selections
+          case Roi.FREEROI:
              PolygonRoi freehandpr = (PolygonRoi) roi;
              int fhx1Points[] = freehandpr.getXCoordinates();
              int fhy1Points[] = freehandpr.getYCoordinates();
              int freehandLength = freehandpr.getNCoordinates();
              GeneralPath freehandpolygon = new GeneralPath(GeneralPath.WIND_EVEN_ODD, freehandLength);
              freehandpolygon.moveTo((fhx1Points[0] + r.x), (fhy1Points[0] + r.y));
-             for (int index = 1; index < freehandLength; index++) {
+             for (int index = 1; index < freehandLength; index++)
                 freehandpolygon.lineTo((fhx1Points[index] + r.x), (fhy1Points[index] + r.y));
-             };
              freehandpolygon.closePath();
              g2.draw(freehandpolygon);
              break;
-          case 5 :
-             //Straight Line Selections
+          case Roi.LINE:
              Line L = (Line) roi;
              Line2D line = new Line2D.Double((double)L.x1, (double)L.y1, (double)L.x2, (double)L.y2);
              g2.draw(line);
              break;
-          case 6 :
-             //Segmented Line Selections
-             PolygonRoi seglinpr = (PolygonRoi) roi;
+          case Roi.POLYLINE:
+              PolygonRoi seglinpr = (PolygonRoi) roi;
              int slx1Points[] = seglinpr.getXCoordinates();
              int sly1Points[] = seglinpr.getYCoordinates();
              int seglinLength = seglinpr.getNCoordinates();
              GeneralPath seglinpolygon = new GeneralPath(GeneralPath.WIND_EVEN_ODD, seglinLength);
              seglinpolygon.moveTo((slx1Points[0] + r.x), (sly1Points[0] + r.y));
-             for (int index = 1; index < seglinLength; index++) {
+             for (int index = 1; index < seglinLength; index++)
                 seglinpolygon.lineTo((slx1Points[index] + r.x), (sly1Points[index] + r.y));
-             };
              g2.draw(seglinpolygon);
              break;
-          case 7 :
-             //Freehand Line Selections
-             PolygonRoi fhlinpr = (PolygonRoi) roi;
+          case Roi.FREELINE:
+              PolygonRoi fhlinpr = (PolygonRoi) roi;
              int fhlx1Points[] = fhlinpr.getXCoordinates();
              int fhly1Points[] = fhlinpr.getYCoordinates();
              int fhlinLength = fhlinpr.getNCoordinates();
              GeneralPath freehandlinpolygon = new GeneralPath(GeneralPath.WIND_EVEN_ODD, fhlinLength);
              freehandlinpolygon.moveTo((fhlx1Points[0] + r.x), (fhly1Points[0] + r.y));
-             for (int index = 1; index < fhlinLength; index++) {
+             for (int index = 1; index < fhlinLength; index++)
                 freehandlinpolygon.lineTo((fhlx1Points[index] + r.x), (fhly1Points[index] + r.y));
-             };
              g2.draw(freehandlinpolygon);
              break;
-          case 8 :
+          case Roi.ANGLE:
              //Angle Selections
              PolygonRoi apr = (PolygonRoi) roi;
              int ax1Points[] = apr.getXCoordinates();
@@ -132,12 +128,12 @@ public class Dotted_Line implements PlugIn {
              int aLength = apr.getNCoordinates();
              GeneralPath apolygon = new GeneralPath(GeneralPath.WIND_EVEN_ODD, aLength);
              apolygon.moveTo((ax1Points[0] + r.x), (ay1Points[0] + r.y));
-             for (int index = 1; index < aLength; index++) {
+             for (int index = 1; index < aLength; index++)
                 apolygon.lineTo((ax1Points[index] + r.x), (ay1Points[index] + r.y));
-             };
              g2.draw(apolygon);
              break;
           }
        imp.setImage(bi);
     }
+
 }
