7219a9139d0c04725f818bb0fc38bf18136d9cb3
Plugin Downloader <plugin.downloader@fiji>
2010-12-09 13:02:10 +0100

    Find 3D Max Benchmarks
        Author:       Wayne Rasband (wsr at nih.gov)
    
        History:      2010/12/01: First release
    
        Source:       ImageJ_Find_Max.java, Byte_Find_Max.java, Imglib_Find_Max.java,
                      ImageScience_Find_Max.java and Pixlib_Find_Max.java.
    
        Installation: Drag and drop ImageJ_Find_Max.java, Byte_Find_Max.java,
                      Imglib_Find_Max.java, ImageScience_Find_Max.java or
                      Pixlib_Find_Max.java onto the "ImageJ" window and compile and run by
                      pressing ctrl-r (File>Compile and Run).
    
        Description:  I developed these plugin to compare the speed of various data type
                      independent pixel access methods. What they do is find the first pixel
                      in an image, stack or hyperstack that contains the maximum value and
                      write to the Log window the maximum value, the location of the pixel
                      and the processing rate in millions of pixels per second. In addition,
                      they create a point selection at the location of the pixel and position
                      the stack or hyperstack so that the image containing the pixel is
                      displayed.
    
                      The Imglib_Find_Max plugin requires Fiji, the ImageScience_Find_Max
                      plugin requires Erik Meijering's imagescience library and the
                      Pixlib_Find_Max plugin requires Dimiter Prodanov's pixlib.jar library.
    
                      The ImageJ_Find_Max plugin runs up to seven times faster on 64-bit
                      versions of Java because the JIT compiler is able to inline the getf()
                      method.
    
                      For best performance, run the plugins by selecting them from the
                      Plugins menu. Run them repeatedly by pressing shift-r (Process>Repeat
                      Command) for even better performance.
    
                      This is the Log window output created by running the ImageJ_Find_Max
                      plugin on a 2.26 Ghz Intel Core 2 Duo with Java 1.6.0_22 (64-bit) on a
                      128MB (512x512x512) stack and then repeatedly pressing shift-r:
    
                          270.6 million pixels/second
                          272.8 million pixels/second
                          767.0 million pixels/second
                          762.6 million pixels/second
                          699.1 million pixels/second
                          771.4 million pixels/second
    
                      This is the output created by running the Imglib_Find_Max plugin on the
                      same stack:
    
                          67.8 million pixels/second
                          73.3 million pixels/second
                          72.9 million pixels/second
                          75.7 million pixels/second
                          77.2 million pixels/second
                          77.2 million pixels/second
    
                       
                      Here is a plot created using this ImageJ macro:
    
                      [Plot]  
                      And here is a plot of the results from running the five benchmarks
                      using a 32-bit version of Java:
    
                      [Plot]
    
    |Plugins | Home |

--- Pixlib_Find_Max.java
+++ Pixlib_Find_Max.java
@@ -1,6 +1,3 @@
-import java.awt.BasicStroke;
-import java.awt.geom.GeneralPath;
-
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
@@ -8,6 +5,8 @@ import ij.gui.ImageCanvas;
 import ij.gui.Toolbar;
 import ij.plugin.PlugIn;
 import ijaux.*;
+import ijaux.compat.ImagePlusCube;
+import ijaux.compat.ImagePlusIndex;
 import ijaux.datatype.Pair;
 import ijaux.datatype.access.*;
 import ijaux.hypergeom.BaseIndex;
@@ -15,90 +14,53 @@ import ijaux.hypergeom.PixelCube;
 import ijaux.iter.IndexedIterator;
  
 public class Pixlib_Find_Max implements PlugIn {
-	private PixLib plib=new PixLib();
+	//private PixLib plib=new PixLib();
  
 	public void run(String arg) {
 			
 		ImagePlus imp = IJ.getImage();
 		long start = System.currentTimeMillis();
-		// converts the ImagePlus to a PixelCube
-		PixelCube<Number,BaseIndex>  pc=plib.cubeFrom(imp, PixLib.BASE_INDEXING);
-		int idx= pc.index();
-		final Access<?> access=pc.getAccess();
 		
-		Pair<Number,Integer> max=  (Pair<Number,Integer>)Pair.of((Number)access.elementFloat(idx),idx);
+		// wraps the ImagePlus to a HyperCube
+		ImagePlusCube ipc=new ImagePlusCube(imp);
+		long time1 = System.currentTimeMillis();
+		
+		int size=Util.cumprod(imp.getDimensions());
+		
+		int idx= 0;
+		Access<?> access=ipc.getAccess();
+		
+	
+		float max=access.elementFloat(idx);
+		int ind=idx;
+		
 		//iterates over the image and finds the 1st global maximum
-		int imgtype=imp.getType();
-		
-		
-		for (IndexedIterator<Number> iter= pc.iterator();iter.hasNext(); iter.next()) {
-			switch (imgtype) {
-			case ImagePlus.GRAY8:
-			case ImagePlus.COLOR_256: 
-			case ImagePlus.GRAY16: {
-				final int c=access.elementInt(idx);
-				if (c> max.first.intValue()  )
-					max=(Pair<Number, Integer>)Pair.of((Number)c,idx);
+		for (idx=0; idx<size; idx++) {
+			final float c=access.elementFloat(idx);
+			if (c>max) {
+				max=c;
+				ind=idx;
 			}
-			case ImagePlus.GRAY32: {
-				final float c=access.elementFloat(idx);
-				if (c > max.first.floatValue()  )
-					max=(Pair<Number, Integer>)Pair.of((Number)c,idx);
-			}
-			
-			}
-			
-			idx= iter.index();
 		}
-	 
-		BaseIndex bi=pc.getIndex();
+		
+		ImagePlusIndex bi=ipc.getIndex();
 		//calculates the image coordinates
-		bi.setIndex(max.second);
+		bi.setIndex(ind);
 		int[] coords=bi.getCoordinates();
-		long time2=System.currentTimeMillis();
 		showTime(imp, start);
 	 
-		//IJ.makePoint(coords[0],coords[1]);
-		drawCross(  coords,   imp);
-		IJ.log("Maximum of "+  access.elementFloat(max.second)  +" found at "+bi);
-		long duration=time2-start;
-		int size=pc.getSize();
-		System.out.println("run time: "+ duration+" ms\n speed " + (float)size/duration +" pix/ms" );
+		IJ.makePoint(coords[0],coords[1]);
+		IJ.log("Maximum of "+  access.elementFloat(ind)  +" found at "+bi);
 
 	}
 
-	void showTime(ImagePlus imp, long start) {
-		int images = imp.getStackSize();
-		double time = (System.currentTimeMillis()-start)/1000.0;
-		IJ.showTime(imp, start, "", images);
-		double mpixels = (double)(imp.getWidth())*imp.getHeight()*images/1000000;
-		IJ.log("\n"+imp);
-		IJ.log(IJ.d2s(mpixels/time,1)+" million pixels/second");
-	}
-
-	void drawCross(int[] coords, ImagePlus imp) {
-		int centerx=coords[0];
-		int centery=coords[1];
-		GeneralPath path = new GeneralPath();
-		final float arm  = 5;
-		float x = (centerx);
-		float y = (centery);
-		path.moveTo(x-arm, y);
-		path.lineTo(x+arm, y);
-		path.moveTo(x, y-arm);
-		path.lineTo(x, y+arm);
-		imp.setOverlay(path,Toolbar.getForegroundColor(), new BasicStroke(2));
-	}
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		new ImageJ();
-		new Pixlib_Find_Max().run(null);
-		
-
-	}
-
+   void showTime(ImagePlus imp, long start) {
+      int images = imp.getStackSize();
+      double time = (System.currentTimeMillis()-start)/1000.0;
+      IJ.showTime(imp, start, "", images);
+      double mpixels = (double)(imp.getWidth())*imp.getHeight()*images/1000000;
+      IJ.log("\n"+imp);
+      IJ.log(IJ.d2s(mpixels/time,1)+" million pixels/second");
+   }
 
  }
