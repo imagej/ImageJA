40a5c2267a0107a2ea3e9c21a26a343e1db50477
Wayne Rasband <wsr@nih.gov>
2011-07-10 21:38:30 +0200

    updated 10.07.2011 21:38

---
+++ ij/plugin/Commands.java
@@ -0,0 +1,160 @@
+package ij.plugin;
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import ij.io.*;
+import ij.plugin.frame.*;
+import ij.text.TextWindow;
+import ij.macro.Interpreter;
+import java.awt.Frame;
+import java.io.File;
+import java.applet.Applet;
+	
+/**	Runs miscellaneous File and Window menu commands. */
+public class Commands implements PlugIn {
+	
+	public void run(String cmd) {
+		if (cmd.equals("new"))
+			new NewImage();
+		else if (cmd.equals("open")) {
+			if (Prefs.useJFileChooser && !IJ.macroRunning())
+				new Opener().openMultiple();
+			else
+				new Opener().open();
+		} else if (cmd.equals("close"))
+			close();
+		else if (cmd.equals("close-all"))
+			closeAll();
+		else if (cmd.equals("save"))
+			save();
+		else if (cmd.equals("ij")) {
+			ImageJ ij = IJ.getInstance();
+			if (ij!=null) ij.toFront();
+		} else if (cmd.equals("tab"))
+			WindowManager.putBehind();
+		else if (cmd.equals("quit")) {
+			ImageJ ij = IJ.getInstance();
+			if (ij!=null) ij.quit();
+		} else if (cmd.equals("revert"))
+			revert();
+		else if (cmd.equals("undo"))
+			undo();
+		else if (cmd.equals("startup"))
+			openStartupMacros();
+    }
+    
+    void revert() {
+    	ImagePlus imp = WindowManager.getCurrentImage();
+		if (imp!=null)
+			imp.revert();
+		else
+			IJ.noImage();
+	}
+
+    void save() {
+    	ImagePlus imp = WindowManager.getCurrentImage();
+		if (imp!=null) {
+			if (imp.getStackSize()>1) {
+				imp.setIgnoreFlush(true);
+				new FileSaver(imp).save();
+				imp.setIgnoreFlush(false);
+			} else
+				new FileSaver(imp).save();
+		} else
+			IJ.noImage();
+	}
+	
+    void undo() {
+    	ImagePlus imp = WindowManager.getCurrentImage();
+		if (imp!=null)
+			Undo.undo();
+		else
+			IJ.noImage();
+	}
+
+	void close() {
+    	ImagePlus imp = WindowManager.getCurrentImage();
+		Frame frame = WindowManager.getFrontWindow();
+		if (frame==null || (Interpreter.isBatchMode() && frame instanceof ImageWindow))
+			closeImage(imp);
+		else if (frame instanceof PlugInFrame)
+			((PlugInFrame)frame).close();
+		else if (frame instanceof TextWindow)
+			((TextWindow)frame).close();
+		else
+			closeImage(imp);
+	}
+
+	void closeAll() {
+    	int[] list = WindowManager.getIDList();
+    	if (list!=null) {
+    		int imagesWithChanges = 0;
+			for (int i=0; i<list.length; i++) {
+				ImagePlus imp = WindowManager.getImage(list[i]);
+				if (imp!=null && imp.changes) imagesWithChanges++;
+			}
+			if (imagesWithChanges>0 && !IJ.macroRunning()) {
+				GenericDialog gd = new GenericDialog("Close All");
+				String msg = null;
+				String pronoun = null;
+				if (imagesWithChanges==1) {
+					msg = "There is one image";
+					pronoun = "it";
+				} else {
+					msg = "There are "+imagesWithChanges+" images";
+					pronoun = "they";
+				}
+				gd.addMessage(msg+" with unsaved changes. If you\nclick \"OK\" "+pronoun
+					+" will be closed without being saved.");
+				gd.showDialog();
+				if (gd.wasCanceled()) return;
+			}
+			for (int i=0; i<list.length; i++) {
+				ImagePlus imp = WindowManager.getImage(list[i]);
+				if (imp!=null) {
+					imp.changes = false;
+					imp.close();
+				}
+			}
+    	}
+    	//Frame[] windows = WindowManager.getNonImageWindows();
+    	//for (int i=0; i<windows.length; i++) {
+    	//	if ((windows[i] instanceof PlugInFrame) && !(windows[i] instanceof Editor))
+    	//		((PlugInFrame)windows[i]).close();
+    	//}
+	}
+
+	void closeImage(ImagePlus imp) {
+		if (imp==null) {
+			IJ.noImage();
+			return;
+		}
+		imp.close();
+		if (Recorder.record && !IJ.isMacro()) {
+			if (Recorder.scriptMode())
+				Recorder.recordCall("imp.close();");
+			else
+				Recorder.record("close");
+			Recorder.setCommand(null); // don't record run("Close")
+		}
+	}
+	
+	// Plugins>Macros>Open Startup Macros command
+	void openStartupMacros() {
+		Applet applet = IJ.getApplet();
+		if (applet!=null) {
+			IJ.run("URL...", "url="+IJ.URL+"/applet/StartupMacros.txt");
+		} else {
+			String path = IJ.getDirectory("macros")+"/StartupMacros.txt";
+			File f = new File(path);
+			if (!f.exists())
+				IJ.error("\"StartupMacros.txt\" not found in ImageJ/macros/");
+			else
+				IJ.open(path);
+		}
+	}
+	
+}
+
+
+
---
+++ ij/process/FloatPolygon.java
@@ -0,0 +1,91 @@
+package ij.process;
+import java.awt.Rectangle;
+
+/** Used by the Roi classes to return float coordinate arrays and to
+	determine if a point is inside or outside of spline fitted selections. */
+public class FloatPolygon {
+	Rectangle bounds;
+
+	/** The number of points. */
+	public int npoints;
+
+	/* The array of x coordinates. */
+	public float xpoints[];
+
+	/* The array of y coordinates. */
+	public float ypoints[];
+
+	/** Constructs an empty FloatPolygon. */ 
+	public FloatPolygon() {
+		npoints = 0;
+		xpoints = new float[10];
+		ypoints = new float[10];
+	}
+
+	/** Constructs a FloatPolygon from x and y arrays. */ 
+	public FloatPolygon(float xpoints[], float ypoints[], int npoints) {
+		this.npoints = npoints;
+		this.xpoints = xpoints;
+		this.ypoints = ypoints;
+	}
+		
+	/** Returns 'true' if the point (x,y) is inside this polygon. This is a Java
+	version of the remarkably small C program by W. Randolph Franklin at
+	http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html#The%20C%20Code
+	*/
+	public boolean contains(float x, float y) {
+		boolean inside = false;
+		for (int i=0, j=npoints-1; i<npoints; j=i++) {
+			if (((ypoints[i]>y)!=(ypoints[j]>y)) &&
+			(x<(xpoints[j]-xpoints[i])*(y-ypoints[i])/(ypoints[j]-ypoints[i])+xpoints[i]))
+			inside = !inside;
+		}
+		return inside;
+	}
+
+	public Rectangle getBounds() {
+		if (npoints==0)
+			return new Rectangle();
+		if (bounds==null)
+			calculateBounds(xpoints, ypoints, npoints);
+		return bounds.getBounds();
+	}
+
+	void calculateBounds(float[] xpoints, float[] ypoints, int npoints) {
+		float minX = Float.MAX_VALUE;
+		float minY = Float.MAX_VALUE;
+		float maxX = Float.MIN_VALUE;
+		float maxY = Float.MIN_VALUE;
+		for (int i=0; i<npoints; i++) {
+			float x = xpoints[i];
+			minX = Math.min(minX, x);
+			maxX = Math.max(maxX, x);
+			float y = ypoints[i];
+			minY = Math.min(minY, y);
+			maxY = Math.max(maxY, y);
+		}
+		int iMinX = (int)Math.floor(minX);
+		int iMinY = (int)Math.floor(minY);
+		bounds = new Rectangle(iMinX, iMinY, (int)(maxX-iMinX+0.5), (int)(maxY-iMinY+0.5));
+	}
+
+	public void addPoint(float x, float y) {
+		if (npoints==xpoints.length) {
+			float[] tmp = new float[npoints*2];
+			System.arraycopy(xpoints, 0, tmp, 0, npoints);
+			xpoints = tmp;
+			tmp = new float[npoints*2];
+			System.arraycopy(ypoints, 0, tmp, 0, npoints);
+			ypoints = tmp;
+		}
+		xpoints[npoints] = x;
+		ypoints[npoints] = y;
+		npoints++;
+		bounds = null;
+	}
+
+	public void addPoint(double x, double y) {
+		addPoint((float)x, (float)y);
+	}
+
+}
