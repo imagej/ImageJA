6ac1b22a28f324503f5db02cb2082db1255fdcf5
Plugin Downloader <plugin.downloader@fiji>
2011-07-12 21:01:29 +0200

    Deinterlace
        Author:          Wayne Rasband (wsr at nih.gov)
    
        History:         2002/05/16: First version
                         2007/09/18: Bug fix
                         2011/07/12: Adds dialog with three choices
    
        Source:          Deinterlace_.java
    
        Installation:    Drag and drop Deinterlace_.class to the "ImageJ" window.
    
        Description:     This plugin deinterlaces an image or stack using one of three methods:
    
                           • Replace odd lines with the average of the adjacent lines.
                           • Replace even lines with the average of the adjacent lines.
                           • Do both of the above and double the number of frames.
    
                              de-interlace.jpg
    
    |Plugins | Home |

--- download/Deinterlace_.java
+++ download/Deinterlace_.java
@@ -2,63 +2,100 @@ import ij.*;
 import ij.process.*;
 import ij.gui.*;
 import java.awt.*;
-import ij.plugin.filter.*;
+import ij.plugin.*;
 
-/** Replaces the odd lines in an image with the average of the two adjacent lines. */
-public class Deinterlace_ implements PlugInFilter {
+/** This plugin de-interlaces an image or stack using one of three methods. */
+public class Deinterlace_ implements PlugIn {
+	static final String[] methods = {"Replace odd lines", "Replace even lines", "Double frames"};
+	static String method = methods[0];
 
-     public int setup(String arg, ImagePlus imp) {
-         if (IJ.versionLessThan("1.28e"))
-            return DONE;
-         else
-            return DOES_ALL-DOES_32+DOES_STACKS;
-     }
+	 public void run(String arg) {
+		ImagePlus imp = IJ.getImage();
+		if (imp.getBitDepth()==32) {
+			IJ.error("32-bit images are not supported");
+			return;
+		}
+		if (!showDialog(imp))
+			return;
+		ImageStack stack = imp.getStack();
+		int n = stack.getSize();
+		if (n==1)
+		Undo.setup(Undo.TRANSFORM, imp);
+		if (method.equals(methods[0])) {
+			for (int i=1; i<=n; i++)
+				deinterlaceOdd(stack.getProcessor(i));
+		} else if (method.equals(methods[1])) {
+			for (int i=1; i<=n; i++)
+				deinterlaceEven(stack.getProcessor(i));
+		} else {
+			for (int i=1; i<=stack.getSize(); i+=2) {
+				ImageProcessor even = stack.getProcessor(i);
+				ImageProcessor odd = even.duplicate();
+				deinterlaceEven(even);
+				deinterlaceOdd(odd);
+				stack.addSlice(null, odd, i);
+			}
+		}
+		imp.setStack(stack);
+	 }
 
-     public void run(ImageProcessor ip) {
-        int width = ip.getWidth();
-        int height = ip.getHeight();
-        int bottom = (height&1)==1?height:height-1;
-        int[]  p1=new int[3],p2=new int[3],avg=new int[3];
-        int samples = ip instanceof ColorProcessor?3:1;
-        for (int y=1; y<bottom; y+=2) {
-             for (int x=0; x<width; x++) {
-                 p1 = ip.getPixel(x, y-1, p1);
-                 p2 = ip.getPixel(x, y+1, p2);
-                 for (int i=0; i<samples; i++) {
-                      avg[i] = ((p1[i]+p2[i])/2);
-                      ip.putPixel(x, y, avg);
-                }
-            }
-        }
-        if (bottom!=height) {
-            int[] aRow = new int[width];
-            ip.getRow(0, height-2, aRow, width);
-            ip.putRow(0, height-1, aRow, width);
-        }
-     }
+	boolean showDialog(ImagePlus imp) {
+		GenericDialog gd = new GenericDialog("De-interlace");
+		gd.addChoice("Method:", methods, method);
+		gd.showDialog();
+		if (gd.wasCanceled()) return false;
+		method = gd.getNextChoice();
+		return true;
+	}
 
-   /* This version that doesn't use preallocted arrays runs about 25% slower
-    public void run(ImageProcessor ip) {
-        int width = ip.getWidth();
-        int height = ip.getHeight();
-        int bottom = (height&1)==1?height:height-1;
-        int[]  p1, p2, avg=new int[3];
-        for (int y=1; y<bottom; y+=2) {
-             for (int x=0; x<width; x++) {
-                 p1 = ip.getPixel(x, y-1, null);
-                 p2 = ip.z(x, y+1, null);
-                 for (int i=0; i<p1.length; i++) {
-                      avg[i] = ((p1[i]+p2[i])/2);
-                      ip.putPixel(x, y, avg);
-                }
-            }
-        }
-        if (bottom!=height) {
-            int[] aRow = new int[width];
-            ip.getRow(0, height-2, aRow, width);
-            ip.putRow(0, height-1, aRow, width);
-        }
-     }
-    */
+	/** Replaces odd lines with average of the two adjacent lines. */
+	 public void deinterlaceOdd(ImageProcessor ip) {
+		int width = ip.getWidth();
+		int height = ip.getHeight();
+		int bottom = (height&1)==1?height:height-1;
+		int[]  p1=new int[3],p2=new int[3],avg=new int[3];
+		int samples = ip instanceof ColorProcessor?3:1;
+		for (int y=1; y<bottom; y+=2) {
+			 for (int x=0; x<width; x++) {
+				 p1 = ip.getPixel(x, y-1, p1);
+				 p2 = ip.getPixel(x, y+1, p2);
+				 for (int i=0; i<samples; i++) {
+					  avg[i] = ((p1[i]+p2[i])/2);
+					  ip.putPixel(x, y, avg);
+				}
+			}
+		}
+		if (bottom!=height) {
+			int[] aRow = new int[width];
+			ip.getRow(0, height-2, aRow, width);
+			ip.putRow(0, height-1, aRow, width);
+		}
+	 }
+
+	/** Replaces even lines with average of the two adjacent lines. */
+	 public void deinterlaceEven(ImageProcessor ip) {
+		int width = ip.getWidth();
+		int height = ip.getHeight();
+		int[] aRow = new int[width];
+		ip.getRow(0, 1, aRow, width);
+		ip.putRow(0, 0, aRow, width);
+		int bottom = (height&1)==1?height-1:height;
+		int[]  p1=new int[3],p2=new int[3],avg=new int[3];
+		int samples = ip instanceof ColorProcessor?3:1;
+		for (int y=2; y<bottom; y+=2) {
+			 for (int x=0; x<width; x++) {
+				 p1 = ip.getPixel(x, y-1, p1);
+				 p2 = ip.getPixel(x, y+1, p2);
+				 for (int i=0; i<samples; i++) {
+					  avg[i] = ((p1[i]+p2[i])/2);
+					  ip.putPixel(x, y, avg);
+				}
+			}
+		}
+		if (bottom!=height) {
+			ip.getRow(0, height-2, aRow, width);
+			ip.putRow(0, height-1, aRow, width);
+		}
+	 }
 
 }
