7b7cd6e8b57e9f5714dd62efda758414cda9783a
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-10-06 13:43:09 -0500

    Back out exception handling related to missing dependencies
    This change was put into Fiji's commit
    
        Start a new class, IJHacker, to eventually cope with plain IJ1
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- ij/IJ.java
+++ ij/IJ.java
@@ -193,17 +193,7 @@ public class IJ {
 		}
 		catch (NoClassDefFoundError e) {
 			int dotIndex = className.indexOf('.');
-			String cause = e.getMessage();
-			int parenIndex = cause.indexOf('(');
-			if (parenIndex >= 1)
-				cause = cause.substring(0, parenIndex - 1);
-			boolean correctClass = cause.endsWith(dotIndex < 0 ?
-				className : className.substring(dotIndex + 1));
-			if (!correctClass && !suppressPluginNotFoundError)
-				error("Plugin " + className +
-					" did not find required class: " +
-					e.getMessage());
-			if (correctClass && dotIndex >= 0)
+			if (dotIndex >= 0)
 				return runUserPlugIn(commandName, className.substring(dotIndex + 1), arg, createNewLoader);
 			if (className.indexOf('_')!=-1 && !suppressPluginNotFoundError)
 				error("Plugin or class not found: \"" + className + "\"\n(" + e+")");
