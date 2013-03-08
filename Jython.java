6390bd2e1cf224b665a338372d00480793bee6e0
Plugin Downloader <plugin.downloader@fiji>
2013-03-07 23:38:15 -0600

    Python (Jython) Interpreter
        History:      2013/02/25: First release
    
        Requires:     ImageJ 1.47m or later
    
        Source:       Jython.java
    
        Installation: This 8MB plugin (Jython.jar), which includes Jython 2.5.3, is
                      automatically downloaded to Image/plugins/jars the first time you run a
                      Jython (".py") script.
    
        Description:  This is the plugin and Jython 2.5.4 interpreter that implement the
                      macro editor's Macros>Evaluate Python command. For more information
                      about Jython (an implementation of Python written in Java), go to
                      www.jython.org.
    
                      The following import statements are automatically added to Jython
                      scripts:
    
                          from ij import *
                          from ij.gui import *
                          from ij.process import *
                          from ij.measure import *
                          from ij.util import *
                          from ij.plugin import *
                          from ij.plugin.filter import *
                          from ij.plugin.frame import *
                          from ij.io import *
                          from java.lang import *
                          from java.awt import *
                          from java.awt.image import *
                          from java.awt.geom import *
                          from java.util import *
                          from java.io import *
    
        See Also:     Python Scripting Tutorial
                      Jython for ImageJ by Example
                      Jython Scripting
    
    )

---
+++ Jython.java
@@ -0,0 +1,98 @@
+import ij.*;
+import ij.plugin.*;
+import ij.io.LogStream;
+import java.io.File;
+import java.util.Properties;
+import org.python.util.PythonInterpreter;
+
+/** This plugin runs Jython scripts. */
+public class Jython extends PlugInInterpreter implements PlugIn, Runnable {
+	public static final String imports =
+		"from ij import *\n"+
+		"from ij.gui import *\n"+
+		"from ij.process import *\n"+
+		"from ij.measure import *\n"+
+		"from ij.util import *\n"+
+		"from ij.plugin import *\n"+
+		"from ij.plugin.filter import *\n"+
+		"from ij.plugin.frame import *\n"+
+		"from ij.io import *\n"+
+		"from java.lang import *\n"+
+		"from java.awt import *\n"+
+		"from java.awt.image import *\n"+
+		"from java.awt.geom import *\n"+
+		"from java.util import *\n"+
+		"from java.io import *\n"+
+		"argument=\"\"\n";
+
+	public static final String name = "Jython";
+	private Thread thread;
+	private String script;
+	private String arg;
+	private String output;
+	private static int counter;
+
+	// run script on separate thread
+	public void run(String script) {
+		if (script.equals(""))
+			return;
+		this.script = script;
+		thread = new Thread(this, name); 
+		thread.setPriority(Math.max(thread.getPriority()-2, Thread.MIN_PRIORITY));
+		thread.start();
+	}
+	
+	// run script on current thread
+	public String run(String script, String arg) {
+		this.script = script;
+		this.arg = arg;
+		run();
+		return null;
+	}
+
+	public String getReturnValue() {
+		return null;
+	}
+
+	public String getImports() {
+		return imports;
+	}
+
+	public String getVersion() {
+		return "1.47m";
+	}
+
+	public String getName() {
+		return "Jython";
+	}
+
+	public void run() {
+		if (counter==0)
+			IJ.showStatus("Starting Jython...");
+		String cacheDir = System.getProperty("user.home")+File.separator+".imagej";
+		File f = new File(cacheDir);
+		if (!f.exists())
+			f.mkdir();
+		Properties props = System.getProperties();
+		props.put("python.home", cacheDir);
+		props.put("python.cachedir.skip", "false");
+		PythonInterpreter py = new PythonInterpreter();
+		LogStream stream = new LogStream();
+		py.setOut(stream);
+		py.setErr(stream);
+		try {
+			py.exec(imports);
+			if (arg!=null && !arg.equals(""))
+				py.exec("argument=\""+arg+"\"\n");
+			py.exec(script); 
+		} catch(Throwable e) {
+			String msg = ""+e;
+			if (!msg.contains(Macro.MACRO_CANCELED))
+				IJ.log(msg);
+		}
+		stream.close();
+		if (counter++==0)
+			IJ.showStatus("");
+	}	
+
+}
